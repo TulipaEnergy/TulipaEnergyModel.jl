7ac8083e9022210fdaed658d2e2565db84d55c89
datejada <diego.tejadaarango@tno.nl>
2023-08-10 11:13:34 +0200

    fix Julia files format

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,20 @@
 using TulipaBulb
 using Documenter
 
-DocMeta.setdocmeta!(TulipaBulb, :DocTestSetup, :(using TulipaBulb); recursive=true)
+DocMeta.setdocmeta!(TulipaBulb, :DocTestSetup, :(using TulipaBulb); recursive = true)
 
 makedocs(;
-    modules=[TulipaBulb],
-    authors="Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
-    repo="https://github.com/TNO-Tulipa/TulipaBulb.jl/blob/{commit}{path}#{line}",
-    sitename="TulipaBulb.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://TNO-Tulipa.github.io/TulipaBulb.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [TulipaBulb],
+    authors = "Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
+    repo = "https://github.com/TNO-Tulipa/TulipaBulb.jl/blob/{commit}{path}#{line}",
+    sitename = "TulipaBulb.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://TNO-Tulipa.github.io/TulipaBulb.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/TNO-Tulipa/TulipaBulb.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/TNO-Tulipa/TulipaBulb.jl", devbranch = "main")
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ const INPUT_FOLDER  = joinpath(@__DIR__, "inputs")
 const OUTPUT_FOLDER = joinpath(@__DIR__, "outputs")
 
 @testset "TulipaBulb.jl" begin
-  objective_function = optimise_investments(INPUT_FOLDER,OUTPUT_FOLDER)
-  @test objective_function ≈ 269238.43825 atol = 1e-5
+    objective_function = optimise_investments(INPUT_FOLDER, OUTPUT_FOLDER)
+    @test objective_function ≈ 269238.43825 atol = 1e-5
 end
