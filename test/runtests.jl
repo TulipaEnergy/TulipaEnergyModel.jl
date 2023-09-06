8b97207534c1d79a5c7343f91634348bc64ac374
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-09-06 10:48:00 +0200

    Use DataFrames to write to csv

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,10 @@ const OUTPUT_FOLDER = joinpath(@__DIR__, "outputs")
     parameters, sets = create_parameters_and_sets_from_file(INPUT_FOLDER)
     solution = optimise_investments(parameters, sets)
     @test solution.objective_value ≈ 269238.43825 atol = 1e-5
-    save_solution_to_file(OUTPUT_FOLDER, solution.v_investment, parameters.p_unit_capacity)
+    save_solution_to_file(
+        OUTPUT_FOLDER,
+        sets.s_assets_investment,
+        solution.v_investment,
+        parameters.p_unit_capacity,
+    )
 end
