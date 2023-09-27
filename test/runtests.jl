ec7cf4ff7a382b58a8e42475e4ba26311c8836f9
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-09-27 15:54:28 +0200

    Use new tiny data and the graph structure in the model

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,10 @@ const INPUT_FOLDER  = joinpath(@__DIR__, "inputs")
 const OUTPUT_FOLDER = joinpath(@__DIR__, "outputs")
 
 @testset "TulipaEnergyModel.jl" begin
-    parameters, sets = create_parameters_and_sets_from_file(INPUT_FOLDER)
-    solution = optimise_investments(parameters, sets)
+    dir = joinpath(INPUT_FOLDER, "tiny")
+    parameters, sets = create_parameters_and_sets_from_file(dir)
+    graph = create_graph(joinpath(dir, "nodes-data.csv"), joinpath(dir, "edges-data.csv"))
+    solution = optimise_investments(graph, parameters, sets)
     @test solution.objective_value ≈ 269238.43825 atol = 1e-5
     save_solution_to_file(
         OUTPUT_FOLDER,
