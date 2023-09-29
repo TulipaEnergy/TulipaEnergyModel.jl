ad95b47316770ca0f503656fed7872ba7c4203c8
Lauren Clisby <lclisby@gmail.com>
2023-09-29 10:28:02 +0200

    Created structs describing CSVs (#122)
    * Created structs describing CSVs
    
    * Adjusted struct and files to match CSV reader

---
+++ src/InputTables.jl
@@ -0,0 +1,44 @@
+struct NodeData
+    id::Int                     # Node ID
+    name::String                # Name of node (geographical?)
+    type::String                # Producer/Consumer - maybe an enum?
+    active::Bool                # Active or decomissioned
+    investable::Bool            # Whether able to invest
+    variable_cost::Float32      # kEUR/MWh
+    investment_cost::Float32    # kEUR/MW/year
+    capacity::Float32           # MW
+    initial_capacity::Float32   # MW
+    peak_demand::Float32        # MW
+end
+
+struct EdgeData
+    id::Int                     # Edge ID
+    carrier::String             # (Optional?) Energy carrier
+    from_node_id::Int           # Node ID
+    to_node_id::Int             # Node ID
+    active::Bool                # Active or decomissioned
+    investable::Bool            # Whether able to invest
+    variable_cost::Float32      # kEUR/MWh
+    investment_cost::Float32    # kEUR/MW/year
+    capacity::Float32           # MW
+    initial_capacity::Float32   # MW
+end
+
+struct EdgeProfiles
+    id::Int                     # Edge ID
+    rep_period::Int
+    time_slice::Int
+    value::Float32              # MW?
+end
+
+struct NodeProfiles
+    id::Int                     # Node ID
+    rep_period::Int
+    time_slice::Int
+    value::Float32              # MW?
+end
+
+struct RepPeriodWeights
+    rep_period::Int
+    weight::Float32
+end
