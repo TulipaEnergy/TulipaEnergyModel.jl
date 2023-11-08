68324a00a04465cd1d7eabe7e70322e39249a181
Greg Neustroev <G.Neustroev@tudelft.nl>
2023-11-08 16:34:18 +0100

    Change constraint to bound

--- src/model.jl
+++ src/model.jl
@@ -191,12 +191,9 @@ function create_model(graph, params, sets; verbose = false, write_lp_file = fals
     )
 
     # Constraints that define a lower bound for flows that are not transport assets
-    # TODO: set lower bound via JuMP API
-    @constraint(
-        model,
-        lower_bound_asset_flow[f ∈ F, rp ∈ RP, B_flow ∈ K_F[(f, rp)]; f ∉ Ft],
-        flow[f, rp, B_flow] ≥ 0
-    )
+    for f ∈ setdiff(F, Ft), rp ∈ RP, B_flow ∈ K_F[(f, rp)]
+        set_lower_bound(flow[f, rp, B_flow], 0.0)
+    end
 
     # Constraints that define bounds for a transport flow Ft
     @expression(
