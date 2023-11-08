fb1a521ad5f32ed01a0427ff94eb7b444fecbd96
Lauren Clisby <lauren.clisby@tno.nl>
2023-11-08 14:19:31 +0100

    Rebase

--- src/run-scenario.jl
+++ src/run-scenario.jl
@@ -1,12 +1,14 @@
 export run_scenario
 
 """
+    run_scenario(input_folder)
     run_scenario(input_folder, output_folder)
 
-Run the scenario in the given input_folder and save the results to the output_folder
+Run the scenario in the given input_folder and return the sets, parameters, and solution.
+If the output_folder is specified, save the sets, parameters, and solution to the output_folder.
 """
 
-function run_scenario(input_folder::AbstractString, output_folder::AbstractString)
+function run_scenario(input_folder::AbstractString)
     parameters, sets = create_parameters_and_sets_from_file(input_folder)
     graph = create_graph(
         joinpath(input_folder, "assets-data.csv"),
@@ -14,33 +16,16 @@ function run_scenario(input_folder::AbstractString, output_folder::AbstractStrin
     )
     model = create_model(graph, parameters, sets)
     solution = solve_model(model)
-    save_solution_to_file(
-        output_folder,
-        sets.assets_investment,
-        solution.assets_investment,
-        parameters.assets_unit_capacity,
-    )
+    return sets, graph, parameters, solution
 end
 
-"""
-    run_scenario(scenario_name)
-
-Run the scenario with the given scenario_name, assuming the folder structure is dir/inputs/scenario_name and dir/outputs/scenario_name
-"""
-function run_scenario(scenario_name::String)
-    input_folder = joinpath(@__DIR__, "inputs", scenario_name)
-    output_folder = joinpath(@__DIR__, "outputs", scenario_name)
-    parameters, sets = create_parameters_and_sets_from_file(input_folder)
-    graph = create_graph(
-        joinpath(input_folder, "assets-data.csv"),
-        joinpath(input_folder, "flows-data.csv"),
-    )
-    model = create_model(graph, parameters, sets)
-    solution = solve_model(model)
+function run_scenario(input_folder::AbstractString, output_folder::AbstractString)
+    sets, graph, parameters, solution = run_scenario(input_folder)
     save_solution_to_file(
         output_folder,
         sets.assets_investment,
         solution.assets_investment,
         parameters.assets_unit_capacity,
     )
+    return sets, graph, parameters, solution
 end
