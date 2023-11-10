5462b8743e72b57430431718ddb195b2f3ad7180
Lauren Clisby <lauren.clisby@tno.nl>
2023-11-10 10:09:02 +0100

    Working on second plot

--- src/plot.jl
+++ src/plot.jl
@@ -7,18 +7,26 @@ function plot_single_flow(
     asset_to::String,
     rp::Int64,
 )
-    rp_partition = sets.time_intervals_per_flow[((asset_from, asset_to), rp)]
+    rp_partition = sets.rp_partitions_flows[((asset_from, asset_to), rp)]
     x = 1:length(rp_partition)
     y = [solution.flow[(asset_from, asset_to), rp, b] for b in rp_partition]
     plot(x, y; title = string("Flow from ", asset_from, " to ", asset_to), legend = false)
 end
 
+#TODO Add more detail in graphics - scaling/coloring based on solution values
 function plot_flow_graph(graph, sets, solution)
     #total_flow_capacity = parameters.flows_unit_capacity + solution.flows_investment       # Need help from Abel to unpack these gracefully
     total_flow_capacity = 10
     gplot(graph; nodelabel = sets.assets, edgelinewidth = total_flow_capacity)
 end
 
-#function plot_assets_balance()
-
+#function plot_assets_balance(solution)
+#    plot_data = solution.assets_investment.data
+#xlabel =
+#    groupedbar(
+#        plot_data,
+#        bar_position = :stack,
+#        xticks = (#:##, xlabel),
+#        label = ["" ""]
+#    )
 #end
