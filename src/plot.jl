27c64092be5e5e8e3e9471156e60f1ca33622c07
Lauren Clisby <lauren.clisby@tno.nl>
2023-11-29 17:24:21 +0100

    Updates for new graph structure

--- src/plot.jl
+++ src/plot.jl
@@ -1,45 +1,53 @@
-export plot_single_flow, plot_flow_graph, plot_assets_balance
+export plot_single_flow, plot_final_flow_graph #, plot_assets_balance
 
-function plot_single_flow(graph, asset_from::String, asset_to::String, rp::Int64)
-    flow_data =
-        plot(x, y; title = string("Flow from ", asset_from, " to ", asset_to), legend = false)
+#%%
+# using TulipaEnergyModel
+# using MetaGraphsNext
+# using Plots
+# using GraphPlot
+# energy_problem = run_scenario("test/inputs/Norse")
+# graph = energy_problem.graph
+# rp = 1
+
+#%%
+function plot_single_flow(graph::MetaGraph, asset_from::String, asset_to::String, rp::Int64)
+    rp_partition = graph[asset_from, asset_to].partitions[rp]
+    x = 1:length(rp_partition)  # time dimension
+    y = [graph[asset_from, asset_to].flow[(1, B)] for B in rp_partition] # flow value
+    plot(
+        x,
+        y;
+        title = string("Flow from ", asset_from, " to ", asset_to, " for RP", rp),
+        legend = false,
+    )
 end
 
-#function plot_single_flow(
-#    solution::NamedTuple,
-#    sets::NamedTuple,
-#    asset_from::String,
-#    asset_to::String,
-#    rp::Int64,
-#)
-#    rp_partition = sets.rp_partitions_flows[((asset_from, asset_to), rp)]
-#    x = 1:length(rp_partition)
-#    y = [solution.flow[(asset_from, asset_to), rp, b] for b in rp_partition]
-#    plot(x, y; title = string("Flow from ", asset_from, " to ", asset_to), legend = false)
-#end
+function plot_single_flow(
+    energy_problem::EnergyProblem,
+    asset_from::String,
+    asset_to::String,
+    rp::Int64,
+)
+    plot_single_flow(energy_problem.graph, asset_from, asset_to, rp)
+end
 
-#function plot_flow_graph(
-#    graph,
-#    sets::NamedTuple,
-#    solution::NamedTuple,
-#    parameters::NamedTuple,
-#)
-#
-#    # Total the final flow capacity by adding initial plus investment
-#    final_flow_cap = copy(parameters.flows_unit_capacity)
-#    for f in sets.flows
-#        if parameters.flows_investable[f]
-#            final_flow_cap[f] += solution.flows_investment[f]
-#        end
-#    end
-#    print(
-#        "The TYPE of final_flow_cap is ",
-#        typeof(final_flow_cap),
-#        " which does not match the Vector that gplot expects.",
-#    )
-#
-#    #gplot(graph; nodelabel = sets.assets, edgelabel = final_flow_cap)
-#end
+# plot_single_flow(graph, "G_imports", "Midgard_CCGT", rp)
+
+function plot_final_flow_graph(graph::MetaGraph)
+    node_labels = labels(graph) |> collect
+    total_flow_cap = [
+        graph[a, b].initial_capacity +
+        graph[a, b].investable * graph[a, b].investment * graph[a, b].unit_capacity for
+        (a, b) in edge_labels(graph)
+    ] # total = initial + investable * investment * unit_capacity
+    gplot(graph; nodelabel = node_labels, edgelabel = total_flow_cap)
+end
+
+function plot_final_flow_graph(energy_problem::EnergyProblem)
+    plot_final_flow_graph(energy_problem.graph)
+end
+
+# plot_flow_graph(energy_problem.graph)
 
 #function plot_assets_balance(solution)
 #    plot_data = solution.assets_investment.data
