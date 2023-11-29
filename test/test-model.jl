fd4178260d7cc6fca0cbafdbf8df31f6c88da4e0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-11-29 10:35:43 +0100

    Allow user to decide whether to use EnergyModel or not (#299)

--- test/test-model.jl
+++ test/test-model.jl
@@ -1,9 +1,9 @@
-@testset "Test that solve_model! throws if model is not created" begin
+@testset "Test that solve_model! throws if model is not created but works otherwise" begin
     energy_problem = create_energy_problem_from_csv_folder(joinpath(INPUT_FOLDER, "Tiny"))
     @test_throws Exception solve_model!(energy_problem)
     @test !energy_problem.solved
     create_model!(energy_problem)
     @test !energy_problem.solved
-    @test solve_model!(energy_problem) === energy_problem
+    solution = solve_model!(energy_problem)
     @test energy_problem.solved
 end
