59bb68d8c986330014bd6efbbff2f5607d52d2f1
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2023-12-13 16:41:23 +0100

    io.jl: read flow graph from json representation of esdl (WIP)

--- src/io.jl
+++ src/io.jl
@@ -1,3 +1,5 @@
+using JSON3
+
 export create_energy_problem_from_csv_folder,
     create_graph_and_representative_periods_from_csv_folder,
     save_solution_to_file,
@@ -398,5 +400,94 @@ function compute_flows_partitions!(partitions, df, u, v, representative_periods)
         else
             _parse_rp_partition(Val(df[j, :specification]), df[j, :partition], rp.time_steps)
         end
+    end
+end
+
+"""
+    read_esdl_json(json_path)
+
+Reads the ESDL JSON file at `json_path` and returns an array of
+from/to node names:
+
+    [(from_name, to_name), (..., ...), ...]
+"""
+function read_esdl_json(json_path)
+    json = JSON3.read(open(f -> read(f, String), json_path))
+    flow_from_json(json)
+end
+
+"""
+    flow_from_json(json)
+
+Returns an array of from/to node names from a JSON document (as parsed
+by JSON3.jl):
+
+    [(from_name, to_name), (..., ...), ...]
+"""
+function flow_from_json(json)
+    """
+        edges(asset)
+
+    Given an asset, find all this_asset -> other_asset edges
+
+    """
+    function edges(asset)
+        [
+            (asset[:name], json_get(json, to_port[Symbol("\$ref")]; trunc = 2)[:name]) for
+            port in asset[:port] if occursin("OutPort", port[:eClass]) for
+            to_port in port[:connectedTo]
+        ]
+    end
+
+    flows = []
+    flow_from_json_impl!(json, flows; find_edge = edges)
+    flows
+end
+
+"""
+    json_get(json, reference; trunc = 0)
+
+Given a JSON document, find the object pointed to by the reference
+(e.g. "//@<key>.<array_idx>/@<key>"); truncate the last `trunc`
+components of the reference.
+"""
+function json_get(json, reference; trunc = 0)
+    function to_idx(token)
+        v = split(token, ".")
+        length(v) > 1 ? [Symbol(v[1]), 1 + parse(Int, v[2])] : [Symbol(v[1])]
+    end
+    # NOTE: index 2:end because there is a leading '/'
+    idx = collect(Iterators.flatten(map(to_idx, split(reference, "/@"))))[2:(end-trunc)]
+    reduce(getindex, idx; init = json)
+end
+
+"""
+    flow_from_json_impl!(json, flows; find_edge)
+
+Find all flows (from/to node names) from a JSON document.
+
+  - `json`: JSON document
+  - `flows`: The flows are returned by appending to this vector
+  - `find_edge`: Function invoked as `find_edge(asset::JSON3.Object)` to find the flows
+    originating from an asset
+"""
+function flow_from_json_impl!(json::JSON3.Object, flows; find_edge)
+    if :asset in keys(json)
+        flow_edges = [find_edge(asset) for asset in json[:asset] if :name in keys(asset)]
+        append!(flows, flow_edges...)
+    end
+
+    if :area in keys(json)
+        flow_from_json_impl!(json[:area], flows; find_edge = find_edge)
+    end
+
+    if :instance in keys(json)
+        flow_from_json_impl!(json[:instance], flows; find_edge = find_edge)
+    end
+end
+
+function flow_from_json_impl!(json::JSON3.Array{JSON3.Object}, flows; find_edge)
+    for json_el in json
+        flow_from_json_impl!(json_el, flows; find_edge = find_edge)
     end
 end
