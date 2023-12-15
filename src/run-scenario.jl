c07ff22577e0b4fc55d01f92c39c5ab91da3e074
Lauren Clisby <lclisby@gmail.com>
2023-12-15 09:13:07 +0100

    Add verbose option to run_scenario (#361)

--- src/run-scenario.jl
+++ src/run-scenario.jl
@@ -9,10 +9,11 @@ The `output_folder` is optional. If it is specified, save the sets, parameters, 
 function run_scenario(
     input_folder::AbstractString,
     output_folder::String = "";
+    verbose = false,
     write_lp_file = false,
 )
     energy_problem = create_energy_problem_from_csv_folder(input_folder)
-    create_model!(energy_problem; write_lp_file = write_lp_file)
+    create_model!(energy_problem; verbose = verbose, write_lp_file = write_lp_file)
     solve_model!(energy_problem)
 
     output_folder == "" ? nothing : save_solution_to_file(output_folder, energy_problem)
