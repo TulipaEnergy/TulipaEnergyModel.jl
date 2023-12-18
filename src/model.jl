e1b79724f77bfad048664d2b721be74c87032469
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-12-18 15:47:31 +0100

    Collect assets and flows in the model to make it faster to evaluate presence in the set

--- src/model.jl
+++ src/model.jl
@@ -62,10 +62,10 @@ function create_model(
     end
 
     ## Sets unpacking
-    A = labels(graph)
-    F = edge_labels(graph)
-    filter_assets(key, value) = Iterators.filter(a -> getfield(graph[a], key) == value, A)
-    filter_flows(key, value) = Iterators.filter(f -> getfield(graph[f...], key) == value, F)
+    A = labels(graph) |> collect
+    F = edge_labels(graph) |> collect
+    filter_assets(key, value) = filter(a -> getfield(graph[a], key) == value, A)
+    filter_flows(key, value) = filter(f -> getfield(graph[f...], key) == value, F)
 
     Ac = filter_assets(:type, "consumer")
     Ap = filter_assets(:type, "producer")
