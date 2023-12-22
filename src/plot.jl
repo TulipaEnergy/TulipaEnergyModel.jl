e0bd044fb041507043574298f10f6f74cebe3624
Lauren Clisby <lauren.clisby@tno.nl>
2023-12-22 17:48:39 +0100

    Improved graph plot

--- src/plot.jl
+++ src/plot.jl
@@ -7,13 +7,13 @@ export plot_single_flow, plot_graph, plot_assets_capacity
 Plot a single flow over a single representative period,
 given a graph or energy problem, the "from" (exporting) asset, the "to" (importing) asset, and the representative period.
 """
-#TODO Rework as Makie
+#TODO Rework using CairoMakie instead of Plots
 function plot_single_flow(graph::MetaGraph, asset_from::String, asset_to::String, rp::Int64)
     rp_partition = graph[asset_from, asset_to].partitions[rp]
     time_dimension = 1:length(rp_partition)
     flow_value = [graph[asset_from, asset_to].flow[(rp, B)] for B in rp_partition]
 
-    plot(
+    Plots.plot(
         time_dimension,
         flow_value;
         title = string("Flow from ", asset_from, " to ", asset_to, " for RP", rp),
@@ -39,12 +39,10 @@ Given a graph or energy problem, plot the graph with the "final" (initial + inve
 represented by the thickness of the graph edges, as well as displayed values.
 """
 function plot_graph(graph::MetaGraph)
-    nodelabelcolor = RGB(0.5, 0.5, 0.5)
-    exportflowcolor = RGB(0, 0.5, 0.5)
-    importflowcolor = RGB(0.5, 0, 0.5)
-    assetflowcolor = RGB(0.5, 0.5, 0.5)
-    nodecentercolor = RGBA(0, 0.4, 0.7, 0.5)
-    nodebordercolor = RGBA(0, 0.9, 1, 0.5)
+    nodelabelcolor = RGB(0.345, 0.164, 0.376)
+    transportflowcolor = RGB(1, 0.647, 0)
+    assetflowcolor = RGB(0.988, 0.525, 0.110)
+    nodecolor = RGBA(0.345, 0.164, 0.376, 0.5)
 
     node_names = labels(graph) |> collect
 
@@ -58,15 +56,14 @@ function plot_graph(graph::MetaGraph)
 
     node_size = []
     node_labels = []
-    border_width = []
     for a in labels(graph)
-        node_capacity = graph[a].initial_capacity
-        node_investment = graph[a].investable * graph[a].investment * graph[a].capacity
-        push!(node_labels, "$a \n $(node_capacity + node_investment)") # "Node Name \n Capacity: ##"
-        push!(node_size, node_capacity)
-        push!(border_width, node_investment)
+        node_total_capacity =
+            graph[a].initial_capacity +
+            graph[a].investable * graph[a].investment * graph[a].capacity
+        push!(node_labels, "$a \n $(node_total_capacity)") # "Node Name \n Capacity: ##"
+        push!(node_size, node_total_capacity)
     end
-    node_size = node_size * 0.8 / maximum(node_size) .+ 0.2
+    node_size = node_size * 98 / maximum(node_size) .+ 5
 
     edge_colors = []
     edge_width = []
@@ -77,7 +74,6 @@ function plot_graph(graph::MetaGraph)
         edge_data = has_edge(graph, e.src, e.dst) ? graph[from, to] : graph[to, from]
 
         if edge_data.is_transport
-            push!(edge_colors, has_edge(graph, e.src, e.dst) ? exportflowcolor : importflowcolor)
             ttc =
                 (
                     if has_edge(graph, e.src, e.dst)
@@ -88,37 +84,38 @@ function plot_graph(graph::MetaGraph)
                 ) + edge_data.investable * edge_data.investment * edge_data.capacity
             push!(edge_labels, string(ttc))
             push!(edge_width, ttc)
+            push!(edge_colors, transportflowcolor)
         else
             push!(edge_colors, assetflowcolor)
             push!(edge_labels, "")
             push!(edge_width, 0)
         end
     end
-    edge_width = edge_width * 0.8 / maximum(edge_width) .+ 0.2 # Normalize edge_width with minimum of 0.2 (just visible)
+    edge_width = edge_width * 0.9 / (maximum(edge_width) == 0 ? 1 : maximum(edge_width)) .+ 0.1 # Normalize edge_width with minimum of 0.1 (just visible)
 
-    #TODO Labels are cut off by edge of figure
-    #TODO Color scheme
-    #TODO Investment strokewidth
-
+    #TODO Show initial vs investment somehow (couldn't get node stroke to work)
     f, ax, p = graphplot(
         temp_graph;
-        node_size = node_size * 100,                # Node center size (scaled)
-        # node_strokewidth = border_width * 100,    # Node border size #TODO Normalizing to self, should normalize to nodelabelsize
-        node_color = nodecentercolor,               # Node center color (initial capacity)
-        node_strokecolor = nodebordercolor,         # Node border color (invested capacity)
-        nlabels = node_labels,
-        nlabels_color = nodelabelcolor,
-        nlabels_fontsize = 10.0,
+        node_size = node_size,
+        node_strokewidth = 0,
+        node_color = nodecolor,
+        ilabels = node_labels,
+        ilabels_color = nodelabelcolor,
+        ilabels_fontsize = 10.0,
         edge_width = edge_width * 10,
         arrow_size = (edge_width .+ 2) * 10,
-        edge_color = edge_colors,                    # Edge color
+        edge_color = edge_colors,
         elabels = edge_labels,
-        elabels_fontsize = 10.0,                     # Max edge label size
-        elabels_color = edge_colors,                 #
-        # layout = Spring(),
+        elabels_fontsize = 10.0,
+        elabels_color = edge_colors,
     )
+
+    #TODO Make these axis adjustments a calculation (so labels aren't cut off)
+    CairoMakie.xlims!(ax, -5.5, 5.5)
+    CairoMakie.ylims!(ax, -5, 6)
     hidedecorations!(ax)
     hidespines!(ax)
+
     return f
 end
 
@@ -133,6 +130,7 @@ end
 Given a graph or energy problem, display a stacked bar graph of
 the initial and invested capacity of each asset (initial + invested = total).
 """
+#TODO Rework using CairoMakie instead of Plots
 function plot_assets_capacity(graph::MetaGraph)
     asset_labels = labels(graph) |> collect
     total_asset_cap = [
@@ -142,7 +140,7 @@ function plot_assets_capacity(graph::MetaGraph)
     investment_cap =
         [graph[a].investable * graph[a].investment * graph[a].capacity for a in labels(graph)]
 
-    plot(
+    Plots.plot(
         [initial_cap, investment_cap];
         seriestype = :bar,
         xticks = (1:length(total_asset_cap), asset_labels),
