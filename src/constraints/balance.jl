3bc8f0d4a4bea3a7a760043ffe716299720d59ea
Lauren Clisby <lauren.clisby@tno.nl>
2024-03-18 17:32:41 +0100

    Move intra rp storage balance to balance.jl

--- src/constraints/balance.jl
+++ src/constraints/balance.jl
@@ -7,6 +7,10 @@ function add_balance_constraints!(
     Ac,
     incoming_flow_highest_in_out_resolution,
     outgoing_flow_highest_in_out_resolution,
+    incoming_flow_lowest_storage_resolution_intra_rp,
+    outgoing_flow_lowest_storage_resolution_intra_rp,
+    df_storage_intra_rp_balance_grouped,
+    storage_level_intra_rp,
 )
     # - consumer balance equation
     df = filter(row -> row.asset ∈ Ac, dataframes[:highest_in_out]; view = true)
@@ -25,5 +29,40 @@ function add_balance_constraints!(
             base_name = "consumer_balance[$(row.asset),$(row.rp),$(row.timesteps_block)]"
         ) for row in eachrow(df)
     ]
-    return nothing
+
+    # - intra representative period (rp) storage balance equation
+    for ((a, rp), sub_df) ∈ pairs(df_storage_intra_rp_balance_grouped)
+        # This assumes an ordering of the time blocks, that is guaranteed inside
+        # construct_dataframes
+        # The storage_inflows have been moved here
+        model[Symbol("storage_intra_rp_balance_$(a)_$(rp)")] = [
+            @constraint(
+                model,
+                storage_level_intra_rp[row.index] ==
+                (
+                    if k > 1
+                        storage_level_intra_rp[row.index-1] # This assumes contiguous index
+                    else
+                        (
+                            if ismissing(graph[a].initial_storage_level)
+                                storage_level_intra_rp[last(sub_df.index)]
+                            else
+                                graph[a].initial_storage_level
+                            end
+                        )
+                    end
+                ) +
+                profile_aggregation(
+                    sum,
+                    graph[a].rep_periods_profiles,
+                    (:inflows, rp),
+                    row.timesteps_block,
+                    0.0,
+                ) * graph[a].storage_inflows +
+                incoming_flow_lowest_storage_resolution_intra_rp[row.index] -
+                outgoing_flow_lowest_storage_resolution_intra_rp[row.index],
+                base_name = "storage_intra_rp_balance[$a,$rp,$(row.timesteps_block)]"
+            ) for (k, row) ∈ enumerate(eachrow(sub_df))
+        ]
+    end
 end
--- src/create-model.jl
+++ src/create-model.jl
@@ -478,43 +478,12 @@ function create_model(graph, representative_periods, dataframes, timeframe; writ
         Ac,
         incoming_flow_highest_in_out_resolution,
         outgoing_flow_highest_in_out_resolution,
+        incoming_flow_lowest_storage_resolution_intra_rp,
+        outgoing_flow_lowest_storage_resolution_intra_rp,
+        df_storage_intra_rp_balance_grouped,
+        storage_level_intra_rp,
     )
 
-    # - intra representative period (rp) storage balance equation
-    for ((a, rp), sub_df) ∈ pairs(df_storage_intra_rp_balance_grouped)
-        # This assumes an ordering of the time blocks, that is guaranteed inside
-        # construct_dataframes
-        # The storage_inflows have been moved here
-        model[Symbol("storage_intra_rp_balance_$(a)_$(rp)")] = [
-            @constraint(
-                model,
-                storage_level_intra_rp[row.index] ==
-                (
-                    if k > 1
-                        storage_level_intra_rp[row.index-1] # This assumes contiguous index
-                    else
-                        (
-                            if ismissing(graph[a].initial_storage_level)
-                                storage_level_intra_rp[last(sub_df.index)]
-                            else
-                                graph[a].initial_storage_level
-                            end
-                        )
-                    end
-                ) +
-                profile_aggregation(
-                    sum,
-                    graph[a].rep_periods_profiles,
-                    (:inflows, rp),
-                    row.timesteps_block,
-                    0.0,
-                ) * graph[a].storage_inflows +
-                incoming_flow_lowest_storage_resolution_intra_rp[row.index] -
-                outgoing_flow_lowest_storage_resolution_intra_rp[row.index],
-                base_name = "storage_intra_rp_balance[$a,$rp,$(row.timesteps_block)]"
-            ) for (k, row) ∈ enumerate(eachrow(sub_df))
-        ]
-    end
     # - inter representative periods (rp) storage balance equation
     for ((a,), sub_df) ∈ pairs(df_storage_inter_rp_balance_grouped)
         # This assumes an ordering of the time blocks, that is guaranteed inside
