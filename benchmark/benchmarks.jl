0278e83031d69eede0ff3ed514a6c4cedfad32a4
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-04-16 10:18:12 +0200

    Change Inf to 86400 in the benchmarks.jl script and benchmark on the HEAD script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -48,12 +48,12 @@ const OUTPUT_FOLDER_BM = mktempdir()
 SUITE["energy_problem"] = BenchmarkGroup()
 SUITE["energy_problem"]["input_and_constructor"] = @benchmarkable begin
     create_energy_problem_from_csv_folder($INPUT_FOLDER_BM)
-end samples = 3 evals = 1 seconds = Inf
+end samples = 3 evals = 1 seconds = 86400
 energy_problem = create_energy_problem_from_csv_folder(INPUT_FOLDER_BM)
 
 SUITE["energy_problem"]["create_model"] = @benchmarkable begin
     create_model!($energy_problem)
-end samples = 3 evals = 1 seconds = Inf
+end samples = 3 evals = 1 seconds = 86400
 create_model!(energy_problem)
 
 # SUITE["energy_problem"]["solve_model"] = @benchmarkable begin
