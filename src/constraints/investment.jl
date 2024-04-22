4250a71a12f94cc1f4623be24112f3321ccbccf1
Ni Wang <ni.wang@tno.nl>
2024-04-22 17:00:37 +0200

    Change investment limits constraint

--- src/constraints/investment.jl
+++ src/constraints/investment.jl
@@ -6,13 +6,21 @@ add_investment_constraints!(graph, Ai, Fi, assets_investment, flows_investment)
 Adds the investment constraints for all asset types and transport flows to the model
 """
 
-function add_investment_constraints!(graph, Ai, Fi, assets_investment, flows_investment)
+function add_investment_constraints!(graph, Ai, Ase, Fi, assets_investment, flows_investment)
 
     # - Maximum (i.e., potential) investment limit for assets
     for a in Ai
-        if graph[a].capacity > 0 && !ismissing(graph[a].investment_limit)
-            bound_value = _find_upper_bound(graph, a)
-            JuMP.set_upper_bound(assets_investment[a], bound_value)
+        if !(a in Ase)
+            if graph[a].capacity > 0 && !ismissing(graph[a].investment_limit)
+                bound_value = _find_upper_bound(graph, a)
+                JuMP.set_upper_bound(assets_investment[a], bound_value)
+            end
+        else # for a in Ase
+            if graph[a].capacity_storage_energy > 0 &&
+               !ismissing(graph[a].investment_limit_storage_energy)
+                bound_value = _find_upper_bound(graph, a)
+                JuMP.set_upper_bound(assets_investment_energy[a], bound_value)
+            end
         end
     end
 
