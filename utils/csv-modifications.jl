efa47bf3e75a02b3d6914c218ee7f339c61bd169
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-08-26 11:27:15 +0200

    Remove empty column in assets-data.csv files
    Due to an extra , in these files, an extra column is created.
    This updates the utils/csv-modifications.jl file to help fix this
    problem, and uses it to remove that extra column.
    
    Closes #754

--- utils/csv-modifications.jl
+++ utils/csv-modifications.jl
@@ -35,7 +35,7 @@ Saves the CSV file at `path`.
 !!! warning
     This will overwrite the `path`.
 """
-function write_tulipa_csv(tulipa_csv, path)
+function write_tulipa_csv(tulipa_csv::TulipaCSV, path)
     open(path, "w") do io
         println(io, join(tulipa_csv.units, ","))
     end
@@ -52,30 +52,105 @@ If `position` is not specified, the column is added to the rightmost place.
 
 The `content` can be a value or a vector of proper size.
 """
-function add_column(tulipa_csv, unit::String, colname, content, position::Int)
+function add_column(tulipa_csv::TulipaCSV, unit::String, colname, content, position::Int)
+    @info "Adding column $colname ($unit) at $position"
     insert!(tulipa_csv.units, position, unit)
     insertcols!(tulipa_csv.csv, position, colname => content)
 end
 
-function add_column(tulipa_csv, colname, content; unit = "", position = size(tulipa_csv.csv, 2) + 1)
+function add_column(
+    tulipa_csv::TulipaCSV,
+    colname,
+    content;
+    unit = "",
+    position = size(tulipa_csv.csv, 2) + 1,
+)
     add_column(tulipa_csv, unit, colname, content, position)
 end
 
 """
-    remove_column(tulipa_csv, colname)
-    remove_column(tulipa_csv, position)
+    unit, content = remove_column(tulipa_csv, colname, position)
+    unit, content = remove_column(tulipa_csv, colname)
+    unit, content = remove_column(tulipa_csv, position)
 
 Removes column `colname` or column at position `position`.
+If both are passed, we check that `colname` happens at `position`.
 """
-function remove_column(tulipa_csv, colname)
-    position = columnindex(tulipa_csv.csv, colname)
-    @assert idx > 0
-    deleteat!(tulipa_csv.units, position)
+function remove_column(tulipa_csv::TulipaCSV, colname, position)
+    @assert colname == names(tulipa_csv.csv)[position]
+    content = tulipa_csv.csv[:, position]
+
+    unit = deleteat!(tulipa_csv.units, position)
     select!(tulipa_csv.csv, Not(colname))
+
+    return unit, content
 end
 
-function remove_column(tulipa_csv, position::Int)
+function remove_column(tulipa_csv::TulipaCSV, colname)
+    position = columnindex(tulipa_csv.csv, colname)
+    return remove_column(tulipa_csv, colname, position)
+end
+
+function remove_column(tulipa_csv::TulipaCSV, position::Int)
     colname = names(tulipa_csv.csv)[position]
-    deleteat!(tulipa_csv.units, position)
-    select!(tulipa_csv.csv, Not(colname))
+    return remove_column(tulipa_csv, colname, position)
+end
+
+"""
+    change_file(f, path)
+    change_file(path) do tulipa_csv
+        # Do stuff to tulipa_csv
+    end
+
+This functions creates a TulipaCSV, applies `f` to it, and then writes it back using `write_tulipa_csv`.
+It's supposed to be used with the `do` syntax:
+
+```julia
+change_file("test/inputs/Norse/assets-data.csv") do tcsv
+    add_column(tcsv, "year", 2030; position=4)
+end
+```
+"""
+function change_file(f, path)
+    tulipa_csv = TulipaCSV(path)
+    f(tulipa_csv)
+    write_tulipa_csv(tulipa_csv, path)
+end
+
+input_files_folders = [
+    [
+        joinpath("test", "inputs", test_input) for
+        test_input in ["Norse", "Storage", "Tiny", "Variable Resolution"]
+    ]
+    joinpath("benchmark", "EU")
+]
+
+"""
+    apply_to_files_named(f, filename)
+    apply_to_files_named(filename) do path
+        # Do stuff to path
+    end
+
+Looks in the input folders defined in `input_files_folders` for a file named `filename` and apply
+the function `f` to it.
+Skips paths that don't exist.
+
+This is supposed to be used with the `do` syntax:
+
+```julia
+apply_to_files_named("assets-data.csv") do path
+    change_file(path) do tcsv
+        add_column(tcsv, "year", 2030; position=4)
+    end
+end
+```
+"""
+function apply_to_files_named(f, filename)
+    for path in (joinpath(folder, filename) for folder in folders)
+        if isfile(path)
+            f(path)
+        else
+            @info "No file $path"
+        end
+    end
 end
