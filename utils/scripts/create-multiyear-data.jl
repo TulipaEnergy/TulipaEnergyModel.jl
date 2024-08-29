00e63f2c8de1d5aad054068ac6c44314dc0ef754
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2024-08-29 11:27:51 +0200

    Create Multi-year Investments test data (#762)
    Create utils/scripts/create-multiyear-data.jl to create this data.
    This will be used by the next PR to implement multiyear input.
    
    Closes #758

---
+++ utils/scripts/create-multiyear-data.jl
@@ -0,0 +1,45 @@
+using DataFrames
+
+include("../csv-modifications.jl")
+
+tiny_folder = "test/inputs/Tiny"
+multiyear_folder = "test/inputs/Multi-year Investments"
+
+# We clean up and copy Tiny
+if isdir(multiyear_folder)
+    rm(multiyear_folder; recursive = true)
+end
+cp(tiny_folder, multiyear_folder)
+
+# Always inside the right folder
+cd(multiyear_folder) do
+    # Extra bad-assets.csv is not necessary
+    rm("bad-assets-data.csv")
+
+    # Copy rows and change year to 2050 in new lines
+    for filename in (
+        "assets-data.csv",
+        "assets-profiles.csv",
+        "flows-data.csv",
+        "flows-profiles.csv",
+        "profiles-rep-periods.csv",
+        "rep-periods-data.csv",
+        "rep-periods-mapping.csv",
+        "timeframe-data.csv",
+        "year-data.csv",
+    )
+        change_file(filename) do tcsv
+            df_2050 = copy(tcsv.csv)
+            df_2050.year .= 2050
+            tcsv.csv = [tcsv.csv; df_2050]
+        end
+    end
+
+    change_file("assets-data.csv") do tcsv
+        tcsv.csv[end, :active] = "false"
+    end
+
+    change_file("flows-data.csv") do tcsv
+        tcsv.csv[end, :active] = "false"
+    end
+end
