3dce1ba7a2e2c8e219353e6d2710e013f038b504
Ni Wang <ni.wang@tno.nl>
2024-09-27 10:15:21 +0200

    Apply review suggestions

--- src/constraints/transport.jl
+++ src/constraints/transport.jl
@@ -18,65 +18,62 @@ function add_transport_constraints!(
 )
 
     ## Expressions used by transport flow constraints
+    # Filter df_flows to flows only for transport assets
+    df = filter([:from, :to] => (from, to) -> (from, to) ∈ Ft, df_flows; view = true)
 
     # - Create upper limit of transport flow
     upper_bound_transport_flow = [
-        if (row.from, row.to) in Ft
-            @expression(
-                model,
-                profile_aggregation(
-                    Statistics.mean,
-                    graph[row.from, row.to].rep_periods_profiles,
-                    row.year,
-                    row.year,
-                    ("availability", row.rep_period),
-                    row.timesteps_block,
-                    1.0,
-                ) *
-                graph[row.from, row.to].capacity *
-                accumulated_flows_export_units[row.year, (row.from, row.to)]
-            )
-        end for row in eachrow(df_flows)
+        @expression(
+            model,
+            profile_aggregation(
+                Statistics.mean,
+                graph[row.from, row.to].rep_periods_profiles,
+                row.year,
+                row.year,
+                ("availability", row.rep_period),
+                row.timesteps_block,
+                1.0,
+            ) *
+            graph[row.from, row.to].capacity *
+            accumulated_flows_export_units[row.year, (row.from, row.to)]
+        ) for row in eachrow(df)
     ]
 
-    # Create lower limit of transport flow
+    # - Create lower limit of transport flow
     lower_bound_transport_flow = [
-        if (row.from, row.to) in Ft
-            @expression(
-                model,
-                profile_aggregation(
-                    Statistics.mean,
-                    graph[row.from, row.to].rep_periods_profiles,
-                    row.year,
-                    row.year,
-                    ("availability", row.rep_period),
-                    row.timesteps_block,
-                    1.0,
-                ) *
-                graph[row.from, row.to].capacity *
-                accumulated_flows_import_units[row.year, (row.from, row.to)]
-            )
-        end for row in eachrow(df_flows)
+        @expression(
+            model,
+            profile_aggregation(
+                Statistics.mean,
+                graph[row.from, row.to].rep_periods_profiles,
+                row.year,
+                row.year,
+                ("availability", row.rep_period),
+                row.timesteps_block,
+                1.0,
+            ) *
+            graph[row.from, row.to].capacity *
+            accumulated_flows_import_units[row.year, (row.from, row.to)]
+        ) for row in eachrow(df)
     ]
 
     ## Constraints that define bounds for a transport flow Ft
-    df = filter([:from, :to] => (from, to) -> (from, to) ∈ Ft, df_flows; view = true)
 
     # - Max transport flow limit
     model[:max_transport_flow_limit] = [
         @constraint(
             model,
-            flow[row.index] ≤ upper_bound_transport_flow[row.index],
+            flow[row.index] ≤ upper_bound_transport_flow[idx],
             base_name = "max_transport_flow_limit[($(row.from),$(row.to)),$(row.year),$(row.rep_period),$(row.timesteps_block)]"
-        ) for row in eachrow(df)
+        ) for (idx, row) in enumerate(eachrow(df))
     ]
 
     # - Min transport flow limit
     model[:min_transport_flow_limit] = [
         @constraint(
             model,
-            flow[row.index] ≥ -lower_bound_transport_flow[row.index],
+            flow[row.index] ≥ -lower_bound_transport_flow[idx],
             base_name = "min_transport_flow_limit[($(row.from),$(row.to)),$(row.year),$(row.rep_period),$(row.timesteps_block)]"
-        ) for row in eachrow(df)
+        ) for (idx, row) in enumerate(eachrow(df))
     ]
 end
