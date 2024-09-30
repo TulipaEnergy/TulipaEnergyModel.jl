3379198f4d11a2cf05c5d09c36382a15777da7db
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-09-30 13:42:34 +0200

    Code review changes

--- src/economic-parameters.jl
+++ src/economic-parameters.jl
@@ -8,7 +8,7 @@ Calculates the annualized cost for each asset, both energy assets and transport 
 # Arguments
 - `discount_rate::Dict`: A dictionary where the key is an `asset` or a pair of assets `(asset1, asset2)` for transport assets, and the value is the discount rate.
 - `economic_lifetime::Dict`: A dictionary where the key is an `asset` or a pair of assets `(asset1, asset2)` for transport assets, and the value is the economic lifetime.
-- `investment_cost::Dict`: A dictionary where the key is an tuple `(year, asset)` or `(year, (asset1, asset2))` for transport assets, and the value is the investment cost.
+- `investment_cost::Dict`: A dictionary where the key is a tuple `(year, asset)` or `(year, (asset1, asset2))` for transport assets, and the value is the investment cost.
 - `years::Array`: An array of years to be considered.
 - `investable_assets::Dict`: A dictionary where the key is a year, and the value is an array of assets that are relevant for that year.
 
@@ -50,16 +50,16 @@ Dict{Tuple{Int64, String}, Float64} with 3 entries:
 # Example for transport assets
 
 ```jldoctest
-discount_rate = Dict(("asset1","asset2") => 0.05, ("asset3","asset4") => 0.07)
+discount_rate = Dict(("asset1", "asset2") => 0.05, ("asset3", "asset4") => 0.07)
 
 economic_lifetime = Dict(("asset1", "asset2") => 10, ("asset3", "asset4") => 15)
 
-investment_cost   = Dict((2021, ("asset1","asset2")) => 1000, (2021, ("asset3","asset4")) => 1500,
-                         (2022, ("asset1","asset2")) => 1100, (2022, ("asset3","asset4")) => 1600)
+investment_cost   = Dict((2021, ("asset1", "asset2")) => 1000, (2021, ("asset3", "asset4")) => 1500,
+                         (2022, ("asset1", "asset2")) => 1100, (2022, ("asset3", "asset4")) => 1600)
 years = [2021, 2022]
 
-investable_assets = Dict(2021 => [("asset1","asset2"), ("asset3","asset4")],
-                         2022 => [("asset1","asset2")])
+investable_assets = Dict(2021 => [("asset1", "asset2"), ("asset3", "asset4")],
+                         2022 => [("asset1", "asset2")])
 
 costs = calculate_annualized_cost(discount_rate, economic_lifetime, investment_cost, years, investable_assets)
 
@@ -191,9 +191,8 @@ function calculate_salvage_value(
     # Create a dict of the years beyond the last milestone year
     end_of_horizon = maximum(years)
     salvage_value_set = Dict(
-        (year, asset) => collect(end_of_horizon+1:year+economic_lifetime[asset]-1) for
-        year in years for asset in investable_assets[year] if
-        year + economic_lifetime[asset] - 1 ≥ end_of_horizon + 1
+        (year, asset) => end_of_horizon+1:year+economic_lifetime[asset]-1 for year in years for
+        asset in investable_assets[year] if year + economic_lifetime[asset] - 1 ≥ end_of_horizon + 1
     )
 
     # Create a dict of salvage values
@@ -292,12 +291,12 @@ salvage_value = Dict(
   (2021, ("asset1", "asset2")) => 759.2,
 )
 
-investment_cost   = Dict((2021, ("asset1","asset2")) => 1000, (2021, ("asset3","asset4")) => 1500,
-                         (2022, ("asset1","asset2")) => 1100, (2022, ("asset3","asset4")) => 1600)
+investment_cost   = Dict((2021, ("asset1", "asset2")) => 1000, (2021, ("asset3", "asset4")) => 1500,
+                         (2022, ("asset1", "asset2")) => 1100, (2022, ("asset3", "asset4")) => 1600)
 years = [2021, 2022]
 
-investable_assets = Dict(2021 => [("asset1","asset2"), ("asset3","asset4")],
-                         2022 => [("asset1","asset2")])
+investable_assets = Dict(2021 => [("asset1", "asset2"), ("asset3", "asset4")],
+                         2022 => [("asset1", "asset2")])
 
 weights = calculate_weight_for_investment_discounts(
     social_rate,
