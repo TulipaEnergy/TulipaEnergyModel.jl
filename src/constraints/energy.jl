f66f9622ec8f3a5151d2117e5d9d596496518d81
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-12-16 15:17:46 +0100

    [energy] Update energy.jl to use attach_constraint

--- src/constraints/energy.jl
+++ src/constraints/energy.jl
@@ -7,42 +7,57 @@ Adds the energy constraints for assets withnin the period blocks of the timefram
 """
 
 function add_energy_constraints!(model, constraints, graph)
-
     ## INTER-TEMPORAL CONSTRAINTS (between representative periods)
 
-    # - Maximum outgoing energy within each period block
-    model[:max_energy_over_clustered_year] = [
-        @constraint(
+    let table_name = :max_energy_over_clustered_year, cons = constraints[table_name]
+        # - Maximum outgoing energy within each period block
+        attach_constraint!(
             model,
-            constraints[:max_energy_over_clustered_year].expressions[:outgoing][row.index] ≤
-            profile_aggregation(
-                sum,
-                graph[row.asset].timeframe_profiles,
-                row.year,
-                row.year,
-                "max_energy",
-                row.period_block_start:row.period_block_end,
-                1.0,
-            ) * (graph[row.asset].max_energy_timeframe_partition[row.year]),
-            base_name = "max_energy_over_clustered_year_limit[$(row.asset),$(row.year),$(row.period_block_start):$(row.period_block_end)]"
-        ) for row in eachrow(constraints[:max_energy_over_clustered_year].indices)
-    ]
+            cons,
+            table_name,
+            [
+                @constraint(
+                    model,
+                    outgoing_flow ≤
+                    profile_aggregation(
+                        sum,
+                        graph[row.asset].timeframe_profiles,
+                        row.year,
+                        row.year,
+                        "max_energy",
+                        row.period_block_start:row.period_block_end,
+                        1.0,
+                    ) * (graph[row.asset].max_energy_timeframe_partition[row.year]),
+                    base_name = "$table_name[$(row.asset),$(row.year),$(row.period_block_start):$(row.period_block_end)]"
+                ) for
+                (row, outgoing_flow) in zip(eachrow(cons.indices), cons.expressions[:outgoing])
+            ],
+        )
+    end
 
-    # - Minimum outgoing energy within each period block
-    model[:min_energy_over_clustered_year] = [
-        @constraint(
+    let table_name = :min_energy_over_clustered_year, cons = constraints[table_name]
+        # - Minimum outgoing energy within each period block
+        attach_constraint!(
             model,
-            constraints[:min_energy_over_clustered_year].expressions[:outgoing][row.index] ≥
-            profile_aggregation(
-                sum,
-                graph[row.asset].timeframe_profiles,
-                row.year,
-                row.year,
-                "min_energy",
-                row.period_block_start:row.period_block_end,
-                1.0,
-            ) * (graph[row.asset].min_energy_timeframe_partition[row.year]),
-            base_name = "min_energy_over_clustered_year_limit[$(row.asset),$(row.year),$(row.period_block_start):$(row.period_block_end)]"
-        ) for row in eachrow(constraints[:min_energy_over_clustered_year].indices)
-    ]
+            cons,
+            table_name,
+            [
+                @constraint(
+                    model,
+                    outgoing_flow ≥
+                    profile_aggregation(
+                        sum,
+                        graph[row.asset].timeframe_profiles,
+                        row.year,
+                        row.year,
+                        "min_energy",
+                        row.period_block_start:row.period_block_end,
+                        1.0,
+                    ) * (graph[row.asset].min_energy_timeframe_partition[row.year]),
+                    base_name = "$table_name[$(row.asset),$(row.year),$(row.period_block_start):$(row.period_block_end)]"
+                ) for
+                (row, outgoing_flow) in zip(eachrow(cons.indices), cons.expressions[:outgoing])
+            ],
+        )
+    end
 end
