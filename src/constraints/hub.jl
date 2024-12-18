b34be57aa928db030782f5ac7d0cc712305ea93e
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-12-18 11:00:10 +0100

    Update constraint files hub.jl, conversion.jl and energy.jl to use attach_constraints (#971)

--- src/constraints/hub.jl
+++ src/constraints/hub.jl
@@ -12,17 +12,20 @@ Adds the hub asset constraints to the model.
 """
 
 function add_hub_constraints!(model, constraints)
-    cons = constraints[:balance_hub]
-    incoming_flow_highest_in_out_resolution = cons.expressions[:incoming]
-    outgoing_flow_highest_in_out_resolution = cons.expressions[:outgoing]
-
     # - Balance constraint (using the lowest temporal resolution)
-    model[:hub_balance] = [
-        @constraint(
+    let table_name = :balance_hub, cons = constraints[:balance_hub]
+        attach_constraint!(
             model,
-            incoming_flow_highest_in_out_resolution[row.index] ==
-            outgoing_flow_highest_in_out_resolution[row.index],
-            base_name = "hub_balance[$(row.asset),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
-        ) for row in eachrow(cons.indices)
-    ]
+            cons,
+            table_name,
+            [
+                @constraint(
+                    model,
+                    incoming_flow == outgoing_flow,
+                    base_name = "$table_name[$(row.asset),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
+                ) for (row, incoming_flow, outgoing_flow) in
+                zip(eachrow(cons.indices), cons.expressions[:incoming], cons.expressions[:outgoing])
+            ],
+        )
+    end
 end
