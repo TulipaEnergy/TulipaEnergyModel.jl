606766ec7b2421246bc0f96b4f396809cfb2de46
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-12-18 09:51:23 +0100

    Create transport table and update transport.jl (#970)

--- src/constraints/transport.jl
+++ src/constraints/transport.jl
@@ -5,77 +5,88 @@ add_transport_constraints!(model, graph, df_flows, flow, Ft, flows_investment)
 
 Adds the transport flow constraints to the model.
 """
-
-function add_transport_constraints!(model, variables, graph, sets)
-    ## unpack from sets
-    Ft = sets[:Ft]
-
+function add_transport_constraints!(model, variables, constraints, graph)
     ## unpack from model
     accumulated_flows_export_units = model[:accumulated_flows_export_units]
     accumulated_flows_import_units = model[:accumulated_flows_import_units]
 
-    ## unpack from variables
-    flows_indices = variables[:flow].indices
-    flow = variables[:flow].container
+    let table_name = :transport_flow_limit, cons = constraints[table_name]
+        var_flow = variables[:flow].container
 
-    ## Expressions used by transport flow constraints
-    # Filter flows_indices to flows only for transport assets
-    transport_flows_indices =
-        filter([:from, :to] => (from, to) -> (from, to) ∈ Ft, flows_indices; view = true)
+        # - Create upper limit of transport flow
+        attach_expression!(
+            cons,
+            :upper_bound_transport_flow,
+            [
+                @expression(
+                    model,
+                    profile_aggregation(
+                        Statistics.mean,
+                        graph[row.from, row.to].rep_periods_profiles,
+                        row.year,
+                        row.year,
+                        ("availability", row.rep_period),
+                        row.time_block_start:row.time_block_end,
+                        1.0,
+                    ) *
+                    graph[row.from, row.to].capacity *
+                    accumulated_flows_export_units[row.year, (row.from, row.to)]
+                ) for row in eachrow(cons.indices)
+            ],
+        )
 
-    # - Create upper limit of transport flow
-    upper_bound_transport_flow = [
-        @expression(
+        # - Create lower limit of transport flow
+        attach_expression!(
+            cons,
+            :lower_bound_transport_flow,
+            [
+                @expression(
+                    model,
+                    profile_aggregation(
+                        Statistics.mean,
+                        graph[row.from, row.to].rep_periods_profiles,
+                        row.year,
+                        row.year,
+                        ("availability", row.rep_period),
+                        row.time_block_start:row.time_block_end,
+                        1.0,
+                    ) *
+                    graph[row.from, row.to].capacity *
+                    accumulated_flows_import_units[row.year, (row.from, row.to)]
+                ) for row in eachrow(cons.indices)
+            ],
+        )
+
+        ## Constraints that define bounds for an investable transport flow
+
+        # - Max transport flow limit
+        attach_constraint!(
             model,
-            profile_aggregation(
-                Statistics.mean,
-                graph[row.from, row.to].rep_periods_profiles,
-                row.year,
-                row.year,
-                ("availability", row.rep_period),
-                row.time_block_start:row.time_block_end,
-                1.0,
-            ) *
-            graph[row.from, row.to].capacity *
-            accumulated_flows_export_units[row.year, (row.from, row.to)]
-        ) for row in eachrow(transport_flows_indices)
-    ]
+            cons,
+            :max_transport_flow_limit,
+            [
+                @constraint(
+                    model,
+                    var_flow[row.var_flow_index] ≤ upper_bound_transport_flow,
+                    base_name = "max_transport_flow_limit[($(row.from),$(row.to)),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
+                ) for (row, upper_bound_transport_flow) in
+                zip(eachrow(cons.indices), cons.expressions[:upper_bound_transport_flow])
+            ],
+        )
 
-    # - Create lower limit of transport flow
-    lower_bound_transport_flow = [
-        @expression(
+        # - Min transport flow limit
+        attach_constraint!(
             model,
-            profile_aggregation(
-                Statistics.mean,
-                graph[row.from, row.to].rep_periods_profiles,
-                row.year,
-                row.year,
-                ("availability", row.rep_period),
-                row.time_block_start:row.time_block_end,
-                1.0,
-            ) *
-            graph[row.from, row.to].capacity *
-            accumulated_flows_import_units[row.year, (row.from, row.to)]
-        ) for row in eachrow(transport_flows_indices)
-    ]
-
-    ## Constraints that define bounds for a transport flow Ft
-
-    # - Max transport flow limit
-    model[:max_transport_flow_limit] = [
-        @constraint(
-            model,
-            flow[row.index] ≤ upper_bound_transport_flow[idx],
-            base_name = "max_transport_flow_limit[($(row.from),$(row.to)),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
-        ) for (idx, row) in enumerate(eachrow(transport_flows_indices))
-    ]
-
-    # - Min transport flow limit
-    model[:min_transport_flow_limit] = [
-        @constraint(
-            model,
-            flow[row.index] ≥ -lower_bound_transport_flow[idx],
-            base_name = "min_transport_flow_limit[($(row.from),$(row.to)),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
-        ) for (idx, row) in enumerate(eachrow(transport_flows_indices))
-    ]
+            cons,
+            :min_transport_flow_limit,
+            [
+                @constraint(
+                    model,
+                    var_flow[row.var_flow_index] ≥ -lower_bound_transport_flow,
+                    base_name = "min_transport_flow_limit[($(row.from),$(row.to)),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
+                ) for (row, lower_bound_transport_flow) in
+                zip(eachrow(cons.indices), cons.expressions[:lower_bound_transport_flow])
+            ],
+        )
+    end
 end
