5bc2a5314d3f4555ff696961f6ad93947b462b1b
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-02-17 16:55:58 +0100

    Remove old profile_aggregation function

--- src/utils.jl
+++ src/utils.jl
@@ -9,26 +9,19 @@ function _check_if_table_exists(connection, table_name)
 end
 
 """
-    profile_aggregation(agg, profiles, key, block, default_value)
+    _profile_aggregate(profiles, tuple_key, time_block, agg_functions, default_value)
 
-Aggregates the `profiles[key]` over the `block` using the `agg` function.
+Aggregates the `profiles[tuple_key]` over the `time_block` using the `agg_function` function.
 If the profile does not exist, uses `default_value` instead of **each** profile value.
 
-`profiles` should be a dictionary of profiles, for instance `graph[a].profiles` or `graph[u, v].profiles`.
-If `profiles[key]` exists, then this function computes the aggregation of `profiles[key]`
-over the range `block` using the aggregator `agg`, i.e., `agg(profiles[key][block])`.
-If `profiles[key]` does not exist, then this substitutes it with a vector of `default_value`s.
-"""
-function profile_aggregation(agg, profiles, year, commission_year, key, block, default_value)
-    if haskey(profiles, year) &&
-       haskey(profiles[year], commission_year) &&
-       haskey(profiles[year][commission_year], key)
-        return agg(profiles[year][commission_year][key][block])
-    else
-        return agg(Iterators.repeated(default_value, length(block)))
-    end
-end
+`profiles` should be a dictionary of profiles, and `tuple_key` should be either
+`(profile_name, year, rep_period)` for the profiles of representative periods
+or `(profile_name, year)` for the profiles over clustered years.
 
+If `profiles[tuple_key]` exists, then this function computes the aggregation of `V = profiles[tuple_key]`
+over the range `time_block` using the aggregator `agg_function`, i.e., `agg_function(V[time_block])`.
+If it does not exist, then `V[time_block]` is substituted by a vector of the corresponding size and `default_value`.
+"""
 function _profile_aggregate(profiles, tuple_key::Tuple, time_block, agg_function, default_value)
     if any(ismissing, tuple_key) || !haskey(profiles, tuple_key)
         return agg_function(Iterators.repeated(default_value, length(time_block)))
