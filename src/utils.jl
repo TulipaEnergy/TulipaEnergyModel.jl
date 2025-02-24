a7c7b3692fd546b4b7ae0b714eacd1da5e734048
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-02-24 10:03:15 +0100

    Improvements to creating expressions attached to constraints (#1033)
    * Update is_charging expressions to use tables
    
    * Create helper function _create_group_table_if_not_exist to use in creating expressions for constraints
    
    * Join two add_expression into a new function in a new file
    
    Function attach_expression_on_constraints_grouping_variables! was
    created in the new file src/expressions/intersection.jl to replace
    add_expression... of is_charing and units_on.
    
    * Pass workspace to attach_expressions_on_constraints...
    
    * Add workspace to other add_expression functions
    
    * Add tests for the new _create_group_table_if_not_exist function

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,10 @@
 # Auxiliary functions to create the model
 
+"""
+    _check_if_table_exists(connection, table_name)
+
+Check if table `table_name` exists in the connection.
+"""
 function _check_if_table_exists(connection, table_name)
     existence_query = DBInterface.execute(
         connection,
@@ -28,4 +33,64 @@ function _profile_aggregate(profiles, tuple_key::Tuple, time_block, agg_function
     end
     profile_value = profiles[tuple_key]
     return agg_function(skipmissing(profile_value[time_block]))
+end
+
+"""
+    _create_group_table_if_not_exist!(
+        connection,
+        table_name,
+        grouped_table_name,
+        group_by_columns,
+        array_agg_columns;
+        rename_columns = Dict(),
+        order_agg_by = "index",
+    )
+
+Create a grouped table grouping the `table_name` into the `grouped_table_name`.
+The `group_by_columns` are the columns that are used in the group by (e.g., asset, year, rep_period),
+and the `array_agg_columns` are the columns that are aggregated into arrays (e.g., index, time_block_start, time_block_end).
+
+It is expected that the original table has an `index` column, which is used in the ordering of the `array_agg_columns`.
+Otherwise, please pass the argument `order_agg_by` with the column that should be used for this ordering.
+
+If one of the columns has to be renamed, use the `rename_columns` dictionary.
+"""
+function _create_group_table_if_not_exist!(
+    connection,
+    table_name,
+    grouped_table_name,
+    group_by_columns,
+    array_agg_columns;
+    rename_columns = Dict(),
+    order_agg_by = :index,
+)
+    if _check_if_table_exists(connection, grouped_table_name)
+        return
+    elseif length(group_by_columns) == 0
+        throw(ArgumentError("`group_by_columns` cannot be empty"))
+    elseif length(array_agg_columns) == 0
+        throw(ArgumentError("`array_agg_columns` cannot be empty"))
+    end
+
+    select_string = join(
+        (
+            "t.$col" * (haskey(rename_columns, col) ? " AS $(rename_columns[col])" : "") for
+            col in group_by_columns
+        ),
+        ", ",
+    )
+    group_by_string = join(("t.$col" for col in group_by_columns), ", ")
+    array_agg_string = join(
+        ("ARRAY_AGG(t.$col ORDER BY $order_agg_by) AS $col" for col in array_agg_columns),
+        ", ",
+    )
+
+    sql_query = "CREATE TEMP TABLE $grouped_table_name AS
+        SELECT $select_string, $array_agg_string,
+        FROM $table_name AS t
+        GROUP BY $group_by_string"
+
+    DuckDB.query(connection, sql_query)
+
+    return
 end
