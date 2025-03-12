58e5ab8c5ff87eb54df68a709938caca8094c75f
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-03-12 14:55:35 +0100

    Add setup steps to benchmark to properly run from scratch (#1092)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,40 +5,34 @@ using DuckDB
 
 const SUITE = BenchmarkGroup()
 
-# The following lines are checking whether this is being called from the `test`.
-# If it is not, then we use a larger dataset
-const INPUT_FOLDER_BM = if isdefined(Main, :Test)
-    joinpath(@__DIR__, "../test/inputs/Norse")
-else
-    joinpath(@__DIR__, "EU")
-end
+function input_setup()
+    # The following lines are checking whether this is being called from the `test`.
+    # If it is not, then we use a larger dataset
+    input_folder = if isdefined(Main, :Test)
+        joinpath(@__DIR__, "../test/inputs/Norse")
+    else
+        joinpath(@__DIR__, "EU")
+    end
 
-const OUTPUT_FOLDER_BM = mktempdir()
-
-SUITE["energy_problem"] = BenchmarkGroup()
-SUITE["energy_problem"]["input_and_constructor"] = @benchmarkable begin
     connection = DBInterface.connect(DuckDB.DB)
     TulipaIO.read_csv_folder(
         connection,
-        $INPUT_FOLDER_BM;
-        schemas = $(TulipaEnergyModel.schema_per_table_name),
+        input_folder;
+        schemas = TulipaEnergyModel.schema_per_table_name,
     )
-    EnergyProblem(connection)
-end samples = 3 evals = 1 seconds = 86400
+    return connection
+end
 
-connection = DBInterface.connect(DuckDB.DB)
-TulipaIO.read_csv_folder(
-    connection,
-    INPUT_FOLDER_BM;
-    schemas = TulipaEnergyModel.schema_per_table_name,
-)
-energy_problem = EnergyProblem(connection)
+function create_model_setup()
+    connection = input_setup()
+    return EnergyProblem(connection)
+end
+
+SUITE["energy_problem"] = BenchmarkGroup()
+SUITE["energy_problem"]["input_and_constructor"] = @benchmarkable begin
+    EnergyProblem(connection)
+end samples = 3 evals = 1 seconds = 86400 setup = (connection = input_setup())
 
 SUITE["energy_problem"]["create_model"] = @benchmarkable begin
-    create_model!($energy_problem)
-end samples = 3 evals = 1 seconds = 86400
-# create_model!(energy_problem)
-
-# SUITE["energy_problem"]["output"] = @benchmarkable begin
-#     export_solution_to_csv_files($OUTPUT_FOLDER_BM, $energy_problem)
-# end
+    create_model!(energy_problem)
+end samples = 3 evals = 1 seconds = 86400 setup = (energy_problem = create_model_setup())
