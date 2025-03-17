82d3d95ac4daef95705abb1780d36c833d72c9e9
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-03-17 14:01:05 +0100

    Make schema const

--- src/input-schemas.jl
+++ src/input-schemas.jl
@@ -1,6 +1,6 @@
 # read schema from file
 
-schema = JSON.parsefile(joinpath(@__DIR__, "input-schemas.json"); dicttype = OrderedDict);
+const schema = JSON.parsefile(joinpath(@__DIR__, "input-schemas.json"); dicttype = OrderedDict);
 
 const schema_per_table_name = OrderedDict(
     schema_key => OrderedDict(key => value["type"] for (key, value) in schema_content) for
