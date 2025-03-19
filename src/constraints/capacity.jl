9cfac65f454f4bf21f75671cfffd72016a319cb7
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-03-19 17:27:18 +0100

    Demo Compare MPS

--- src/constraints/capacity.jl
+++ src/constraints/capacity.jl
@@ -23,13 +23,14 @@ function add_capacity_constraints!(connection, model, expressions, constraints, 
                         model,
                         row.capacity * sum(
                             begin
-                                availability_agg = _profile_aggregate(
-                                    profiles.rep_period,
-                                    (avail_profile_name, row.year, row.rep_period),
-                                    row.time_block_start:row.time_block_end,
-                                    Statistics.mean,
-                                    1.0,
-                                )
+                                availability_agg =
+                                    _profile_aggregate(
+                                        profiles.rep_period,
+                                        (avail_profile_name, row.year, row.rep_period),
+                                        row.time_block_start:row.time_block_end,
+                                        Statistics.mean,
+                                        1.0,
+                                    ) * (rand() < 1e-2 ? 1.1 : 1.0)
                                 availability_agg * expr_avail[avail_id]
                             end for (avail_profile_name, avail_id) in
                             zip(row.avail_profile_name, row.avail_indices)
