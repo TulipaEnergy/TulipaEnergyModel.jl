9d5a41115ec59574d2fb428c577b4630ee60ae82
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-03-20 11:59:08 +0100

    Update utils/scripts/model-mps-common.jl
    Co-authored-by: Diego Alejandro Tejada Arango <12887482+datejada@users.noreply.github.com>

--- utils/scripts/model-mps-common.jl
+++ utils/scripts/model-mps-common.jl
@@ -5,7 +5,8 @@ function create_mps(input_folder, mps_folder)
     schemas = TulipaEnergyModel.schema_per_table_name
     TulipaIO.read_csv_folder(con, input_folder; schemas)
     model_file_name = joinpath(mps_folder, basename(input_folder) * ".mps")
-    TulipaEnergyModel.run_scenario(con; model_file_name, show_log = false)
+    energy_problem = TulipaEnergyModel.EnergyProblem(con)
+    TulipaEnergyModel.create_model!(energy_problem; model_file_name)
 
     return nothing
 end
