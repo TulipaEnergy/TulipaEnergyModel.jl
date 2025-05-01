648f1b8c38d5d6ca8c260f3c09f61f8b57708d93
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-05-01 16:04:04 +0200

    Improve tests of the data preparation

--- test/test-data-preparation.jl
+++ test/test-data-preparation.jl
@@ -1,4 +1,4 @@
-@testset "create_merged_tables!" begin
+@testset "Test creating tables for lowest and highest resolution in different cases" begin
     # Setup a temporary DuckDB connection
     connection = DuckDB.DB()
 
@@ -9,20 +9,20 @@
             "CREATE TABLE flow_time_resolution_rep_period
                 (from_asset STRING,
                  to_asset STRING,
-                 year INT,
-                 rep_period INT,
-                 time_block_start INT,
-                 time_block_end INT)
+                 year INTEGER,
+                 rep_period INTEGER,
+                 time_block_start INTEGER,
+                 time_block_end INTEGER)
             ",
         )
         DBInterface.execute(
             connection,
             "CREATE TABLE asset_time_resolution_rep_period
                 (asset STRING,
-                 year INT,
-                 rep_period INT,
-                 time_block_start INT,
-                 time_block_end INT)
+                 year INTEGER,
+                 rep_period INTEGER,
+                 time_block_start INTEGER,
+                 time_block_end INTEGER)
             ",
         )
         DBInterface.execute(
@@ -32,18 +32,28 @@
                  flow_1_to_asset STRING,
                  flow_2_from_asset STRING,
                  flow_2_to_asset STRING,
-                 milestone_year INT)
+                 milestone_year INTEGER)
             ",
         )
 
-        # Insert mock data into the tables
+        DBInterface.execute(
+            connection,
+            "CREATE TABLE rep_periods_data
+                (num_timesteps INTEGER,
+                 rep_period INTEGER,
+                 resolution DOUBLE,
+                 year INTEGER)
+            ",
+        )
+
+        # Insert mock data into the tables (example data of a Multiple Inputs and Outputs - MIMO)
         DBInterface.execute(
             connection,
             "INSERT INTO flow_time_resolution_rep_period
-                 VALUES ('input_1', 'asset', 2025, 1, 1, 3),
+                 VALUES ('input_1', 'asset', 2025, 1, 1, 1),
                         ('input_2', 'asset', 2025, 1, 1, 2),
-                        ('asset', 'output_1', 2025, 1, 1, 1),
-                        ('asset', 'output_2', 2025, 1, 1, 2)
+                        ('asset', 'output_1', 2025, 1, 1, 3),
+                        ('asset', 'output_2', 2025, 1, 1, 4)
             ",
         )
         DBInterface.execute(
@@ -59,46 +69,130 @@
                        ('asset', 'output_1', 'asset', 'output_2', 2025),
             ",
         )
+        DBInterface.execute(
+            connection,
+            "INSERT INTO rep_periods_data
+                VALUES (5, 1, 1.0, 2025)
+            ",
+        )
 
-        # Call the function to test
+        # Call the create_merge_tables! function to test
         TulipaEnergyModel.create_merged_tables!(connection)
 
         # Verify the results in the merged tables
         merged_in_flows = TulipaIO.get_table(connection, "merged_in_flows")
-        @test DataFrames.nrow(merged_in_flows) == 4
-        @test DataFrames.ncol(merged_in_flows) == 5
-        @test issubset(merged_in_flows.asset, ["asset" "output_1" "output_2"])
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 1),
+            ("asset", 2025, 1, 1, 2),
+            ("output_1", 2025, 1, 1, 3),
+            ("output_2", 2025, 1, 1, 4),
+        ]
+        @test _test_rows_exist(rows_to_test, merged_in_flows) |> all
 
         merged_out_flows = TulipaIO.get_table(connection, "merged_out_flows")
-        @test DataFrames.nrow(merged_out_flows) == 4
-        @test DataFrames.ncol(merged_out_flows) == 5
-        @test issubset(merged_out_flows.asset, ["asset" "input_1" "input_2"])
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 3),
+            ("asset", 2025, 1, 1, 4),
+            ("input_1", 2025, 1, 1, 1),
+            ("input_2", 2025, 1, 1, 2),
+        ]
+        @test _test_rows_exist(rows_to_test, merged_out_flows) |> all
 
         merged_assets_and_out_flows = TulipaIO.get_table(connection, "merged_assets_and_out_flows")
-        @test DataFrames.nrow(merged_assets_and_out_flows) == 5
-        @test DataFrames.ncol(merged_assets_and_out_flows) == 5
-        @test issubset(merged_assets_and_out_flows.asset, ["asset" "input_1" "input_2"])
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 3),
+            ("asset", 2025, 1, 1, 4),
+            ("asset", 2025, 1, 1, 5),
+            ("input_1", 2025, 1, 1, 1),
+            ("input_2", 2025, 1, 1, 2),
+        ]
+        @test _test_rows_exist(rows_to_test, merged_assets_and_out_flows) |> all
 
         merged_all_flows = TulipaIO.get_table(connection, "merged_all_flows")
-        @test DataFrames.nrow(merged_all_flows) == 7
-        @test DataFrames.ncol(merged_all_flows) == 5
-        @test issubset(merged_all_flows.asset, ["asset" "input_1" "input_2" "output_1" "output_2"])
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 1),
+            ("asset", 2025, 1, 1, 2),
+            ("asset", 2025, 1, 1, 3),
+            ("asset", 2025, 1, 1, 4),
+            ("input_1", 2025, 1, 1, 1),
+            ("input_2", 2025, 1, 1, 2),
+            ("output_1", 2025, 1, 1, 3),
+            ("output_2", 2025, 1, 1, 4),
+        ]
+        @test _test_rows_exist(rows_to_test, merged_all_flows) |> all
 
         merged_all = TulipaIO.get_table(connection, "merged_all")
-        @test DataFrames.nrow(merged_all) == 8
-        @test DataFrames.ncol(merged_all) == 5
-        @test issubset(merged_all.asset, ["asset" "input_1" "input_2" "output_1" "output_2"])
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 1),
+            ("asset", 2025, 1, 1, 2),
+            ("asset", 2025, 1, 1, 3),
+            ("asset", 2025, 1, 1, 4),
+            ("asset", 2025, 1, 1, 5),
+            ("input_1", 2025, 1, 1, 1),
+            ("input_2", 2025, 1, 1, 2),
+            ("output_1", 2025, 1, 1, 3),
+            ("output_2", 2025, 1, 1, 4),
+        ]
+        @test _test_rows_exist(rows_to_test, merged_all) |> all
 
         merged_flows_relationship = TulipaIO.get_table(connection, "merged_flows_relationship")
-        @test DataFrames.nrow(merged_flows_relationship) == 3
-        @test DataFrames.ncol(merged_flows_relationship) == 5
-        @test unique(merged_flows_relationship.asset) == ["asset"]
+        rows_to_test = [
+            ("asset_output_1_asset_output_2", 2025, 1, 1, 3),
+            ("asset_output_1_asset_output_2", 2025, 1, 1, 4),
+            ("asset_output_1_input_1_asset", 2025, 1, 1, 1),
+            ("asset_output_1_input_1_asset", 2025, 1, 1, 3),
+            ("input_1_asset_asset_output_1", 2025, 1, 1, 1),
+            ("input_1_asset_asset_output_1", 2025, 1, 1, 3),
+            ("input_1_asset_input_2_asset", 2025, 1, 1, 1),
+            ("input_1_asset_input_2_asset", 2025, 1, 1, 2),
+        ]
+        @test _test_rows_exist(rows_to_test, merged_flows_relationship) |> all
+
+        # Call the create_lowest_resolution_table! function to test
+        TulipaEnergyModel.create_lowest_resolution_table!(connection)
+
+        # Verify the results in the lowest resolution tables
+        t_lowest_all = TulipaIO.get_table(connection, "t_lowest_all")
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 5),
+            ("input_1", 2025, 1, 1, 1),
+            ("input_2", 2025, 1, 1, 2),
+            ("output_1", 2025, 1, 1, 3),
+            ("output_2", 2025, 1, 1, 4),
+        ]
+        @test _test_rows_exist(rows_to_test, t_lowest_all) |> all
+
+        t_lowest_all_flows = TulipaIO.get_table(connection, "t_lowest_all_flows")
+        rows_to_test = [
+            ("asset", 2025, 1, 1, 4),
+            ("input_1", 2025, 1, 1, 1),
+            ("input_2", 2025, 1, 1, 2),
+            ("output_1", 2025, 1, 1, 3),
+            ("output_2", 2025, 1, 1, 4),
+        ]
+        @test _test_rows_exist(rows_to_test, t_lowest_all_flows) |> all
+
+        t_lowest_flows_relationship = TulipaIO.get_table(connection, "t_lowest_flows_relationship")
+        rows_to_test = [
+            ("asset_output_1_asset_output_2", 2025, 1, 1, 4),
+            ("asset_output_1_input_1_asset", 2025, 1, 1, 3),
+            ("input_1_asset_asset_output_1", 2025, 1, 1, 3),
+            ("input_1_asset_input_2_asset", 2025, 1, 1, 2),
+        ]
+        @test _test_rows_exist(rows_to_test, t_lowest_flows_relationship) |> all
+
+        # Call the create_lowest_resolution_table! function to test
+        TulipaEnergyModel.create_highest_resolution_table!(connection)
+
+        # test that the final number of tables is correct
+        @test DataFrames.nrow(TulipaIO.show_tables(connection)) == 17
 
     finally
         # Clean up the temporary database
         DBInterface.execute(connection, "DROP TABLE IF EXISTS flow_time_resolution_rep_period")
         DBInterface.execute(connection, "DROP TABLE IF EXISTS asset_time_resolution_rep_period")
         DBInterface.execute(connection, "DROP TABLE IF EXISTS flows_relationships")
+        DBInterface.execute(connection, "DROP TABLE IF EXISTS rep_periods_data")
         DBInterface.execute(connection, "DROP TABLE IF EXISTS merged_in_flows")
         DBInterface.execute(connection, "DROP TABLE IF EXISTS merged_out_flows")
         DBInterface.execute(connection, "DROP TABLE IF EXISTS merged_assets_and_out_flows")
--- test/utils.jl
+++ test/utils.jl
@@ -17,3 +17,17 @@ function _storage_fixture()
     _read_csv_folder(connection, joinpath(@__DIR__, "inputs", "Storage"))
     return connection
 end
+
+function _test_rows_exist(rows_to_test::Vector, table_to_test::DataFrame)
+    return [
+        any(
+            row ->
+                row.asset == asset &&
+                    row.year == year &&
+                    row.rep_period == rep_period &&
+                    row.time_block_start == time_block_start &&
+                    row.time_block_end == time_block_end,
+            eachrow(table_to_test),
+        ) for (asset, year, rep_period, time_block_start, time_block_end) in rows_to_test
+    ]
+end
