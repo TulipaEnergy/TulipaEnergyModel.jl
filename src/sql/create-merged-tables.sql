50c8639eb73784616bbae2ee5649b19325595673
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-05-07 16:27:25 +0200

    Update SQL to create new merged table

--- src/sql/create-merged-tables.sql
+++ src/sql/create-merged-tables.sql
@@ -61,22 +61,15 @@ from
     merged_all_flows
 ;
 
--- merged table for flows relationships
--- 1. create a new column to store the flows relationship
-alter table flows_relationships
-    add flows_relationship STRING
-;
--- 2. add the values to the new flows relationship column
-update flows_relationships
-set flows_relationship = CONCAT(flow_1_from_asset, '_',
-                                flow_1_to_asset, '_',
-                                flow_2_from_asset, '_',
-                                flow_2_to_asset)
-;
--- 3. get the resolution of the flows that are in the relationship
+-- merged table for flows relationships:
+-- 1. Define the asset as the combination of the two flows (i.e., CONCAT)
+-- 2. Get the resolution of the flows that are in the relationship (i.e., JOIN)
 create or replace temp table merged_flows_relationship as
     select distinct
-        fr.flows_relationship AS asset,
+        CONCAT(fr.flow_1_from_asset, '_',
+               fr.flow_1_to_asset, '_',
+               fr.flow_2_from_asset, '_',
+               fr.flow_2_to_asset) as asset,
         ftrrp.year,
         ftrrp.rep_period,
         ftrrp.time_block_start,
