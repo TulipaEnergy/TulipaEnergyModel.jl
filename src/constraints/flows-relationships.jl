328bb0cc27fcb5a1b2cb045e9918818b85542f91
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-05-16 19:17:04 +0200

    Add duration to the constraint

--- src/constraints/flows-relationships.jl
+++ src/constraints/flows-relationships.jl
@@ -9,28 +9,29 @@ function add_flows_relationships_constraints!(connection, model, variables, cons
     cons = constraints[:flows_relationships]
     flows = variables[:flow]
 
+    # get the indices of the flows that are used in the constraints from flow_1 and flow_2
     grouped_indices_flow_1 =
         _get_flow_aggregated_indices(connection, "flow_1_from_asset", "flow_1_to_asset")
     grouped_indices_flow_2 =
         _get_flow_aggregated_indices(connection, "flow_2_from_asset", "flow_2_to_asset")
 
     # Expressions used by flows relationships
-    attach_expression!(
-        cons,
-        :flow_1,
-        JuMP.AffExpr[
-            @expression(model, sum(flows.container[id] for id in row.matching_ids)) for
-            row in grouped_indices_flow_1
-        ],
-    )
-    attach_expression!(
-        cons,
-        :flow_2,
-        JuMP.AffExpr[
-            @expression(model, sum(flows.container[id] for id in row.matching_ids)) for
-            row in grouped_indices_flow_2
-        ],
-    )
+    for (expression_name, grouped_indices) in
+        zip((:flow_1, :flow_2), (grouped_indices_flow_1, grouped_indices_flow_2))
+        attach_expression!(
+            cons,
+            expression_name,
+            JuMP.AffExpr[
+                @expression(
+                    model,
+                    sum(
+                        row.duration[i] * flows.container[id] for
+                        (i, id) in enumerate(row.matching_ids)
+                    )
+                ) for row in grouped_indices
+            ],
+        )
+    end
 
     # - Constraints between two flows (using the lowest temporal resolution)
     attach_constraint!(
@@ -64,15 +65,18 @@ function _get_flow_aggregated_indices(connection, from_asset::String, to_asset::
         connection,
         "SELECT
              indices.id,
-             COALESCE(ARRAY_AGG(flow.id), []::BIGINT[]) AS matching_ids
+             COALESCE(ARRAY_AGG(flow.id), []::BIGINT[]) AS matching_ids,
+             COALESCE(ARRAY_AGG(LEAST(indices.time_block_end, flow.time_block_end) -
+                                GREATEST(indices.time_block_start, flow.time_block_start) +
+                                1), []::BIGINT[]) AS duration
          FROM cons_flows_relationships AS indices
          LEFT JOIN var_flow AS flow
              ON  indices.$(from_asset) = flow.from_asset
              AND indices.$(to_asset) = flow.to_asset
              AND indices.year = flow.year
              AND indices.rep_period = flow.rep_period
-             AND flow.time_block_start
-                    BETWEEN indices.time_block_start AND indices.time_block_end
+             AND flow.time_block_start <= indices.time_block_end
+             AND flow.time_block_end >= indices.time_block_start
          GROUP BY
              indices.id
          ORDER BY
