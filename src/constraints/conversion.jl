3eb47f6ad50cab969ba6a55f19af30c7398f6348
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-06-02 22:37:35 +0200

    Update conversion balance constraint with new efficiency parameter

--- src/constraints/conversion.jl
+++ src/constraints/conversion.jl
@@ -1,13 +1,14 @@
 export add_conversion_constraints!
 
 """
-    add_conversion_constraints!(model, constraints)
+    add_conversion_constraints!(connection, model, constraints)
 
 Adds the conversion asset constraints to the model.
 """
-function add_conversion_constraints!(model, constraints)
+function add_conversion_constraints!(connection, model, constraints)
     # - Balance constraint (using the lowest temporal resolution)
     let table_name = :balance_conversion, cons = constraints[table_name]
+        indices = _append_conversion_data_to_indices(connection, table_name)
         attach_constraint!(
             model,
             cons,
@@ -15,13 +16,28 @@ function add_conversion_constraints!(model, constraints)
             [
                 @constraint(
                     model,
-                    incoming_flow == outgoing_flow,
+                    row.conversion_efficiency * incoming_flow == outgoing_flow,
                     base_name = "conversion_balance[$(row.asset),$(row.year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
                 ) for (row, incoming_flow, outgoing_flow) in
-                zip(cons.indices, cons.expressions[:incoming], cons.expressions[:outgoing])
+                zip(indices, cons.expressions[:incoming], cons.expressions[:outgoing])
             ],
         )
     end
 
     return
+end
+
+function _append_conversion_data_to_indices(connection, table_name)
+    return DuckDB.query(
+        connection,
+        "SELECT
+            cons.*,
+            asset_commission.conversion_efficiency,
+        FROM cons_$table_name AS cons
+        LEFT JOIN asset_commission
+            ON cons.asset = asset_commission.asset
+            AND cons.year = asset_commission.commission_year
+        ORDER BY cons.id
+        ",
+    )
 end
