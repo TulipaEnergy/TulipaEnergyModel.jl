404ebbb9f8b637a453a0596cb05fe01a6e48f233
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-09-02 12:42:49 +0200

    [WIP] Rolling horizon

--- src/structures.jl
+++ src/structures.jl
@@ -245,7 +245,7 @@ mutable struct ProfileLookup
     rep_period::Dict{Tuple{String,Int32,Int32},ProfileWithRollingHorizon}
 
     # over_clustered_year[(asset, year)]
-    over_clustered_year::Dict{Tuple{String,Int32},Vector{Float64}}
+    over_clustered_year::Dict{Tuple{String,Int32},ProfileWithRollingHorizon}
 end
 
 """
