f6044d6d1372d6438a5313f7040940c471b0816d
Diego Alejandro Tejada Arango <dtejada@comillas.edu>
2026-08-14 15:51:16 +0200

    Add new variables to limit the accumulated storage intra rep period
    Use CTE sql style in the new variables creation
    
    Fix order variable creation

--- src/variables/storage.jl
+++ src/variables/storage.jl
@@ -9,6 +9,10 @@ function add_storage_variables!(connection, model, variables)
     storage_level_inter_period_indices = variables[:storage_level_inter_period].indices
     accumulated_storage_level_intra_rep_period_indices =
         variables[:accumulated_storage_level_intra_rep_period].indices
+    max_storage_level_increase_intra_rep_period_indices =
+        variables[:max_storage_level_increase_intra_rep_period].indices
+    max_storage_level_decrease_intra_rep_period_indices =
+        variables[:max_storage_level_decrease_intra_rep_period].indices
     is_charging_indices = variables[:is_charging].indices
 
     variables[:storage_level_intra_rep_period].container = [
@@ -32,6 +36,22 @@ function add_storage_variables!(connection, model, variables)
             model,
             base_name = "accumulated_storage_level_intra_rep_period[$(row.asset),$(row.milestone_year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
         ) for row in accumulated_storage_level_intra_rep_period_indices
+    ]
+
+    variables[:max_storage_level_increase_intra_rep_period].container = [
+        @variable(
+            model,
+            lower_bound = 0.0,
+            base_name = "max_storage_level_increase_intra_rep_period[$(row.asset),$(row.milestone_year),$(row.rep_period)]"
+        ) for row in max_storage_level_increase_intra_rep_period_indices
+    ]
+
+    variables[:max_storage_level_decrease_intra_rep_period].container = [
+        @variable(
+            model,
+            lower_bound = 0.0,
+            base_name = "max_storage_level_decrease_intra_rep_period[$(row.asset),$(row.milestone_year),$(row.rep_period)]"
+        ) for row in max_storage_level_decrease_intra_rep_period_indices
     ]
 
     variables[:is_charging].container = [
