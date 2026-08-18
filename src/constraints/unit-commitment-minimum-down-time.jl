84df2b89507627986ed426adea2817de647b9062
Uros Gluscevic <U.Gluscevic@student.tudelft.nl>
2026-08-18 23:10:58 +0200

    Replace iteration in minimum up/down time constraint creation with SQL query to improve efficiency

--- src/constraints/unit-commitment-minimum-down-time.jl
+++ src/constraints/unit-commitment-minimum-down-time.jl
@@ -8,30 +8,16 @@ Adds the minimum down time constraints to the model.
 function add_minimum_down_time_constraints!(connection, model, variables, expressions, constraints)
     #Minimum down time with simple investment strategy
     let table_name = :minimum_down_time_aggregated_vintage_method,
-        units_on = variables[:units_on].container
-
+        units_on = variables[:units_on].container,
+        shut_down = variables[:shut_down].container,
+        expr_avail_aggregated_vintage_method =
+            expressions[:available_asset_units_aggregated_vintage_method].expressions[:assets],
         cons = constraints[table_name]
 
-        asset_year_rep_period_dict = Dict()
-        for row in cons.indices
-            key = "$(row.asset),$(row.milestone_year),$(row.rep_period)"
-            if (!haskey(asset_year_rep_period_dict, key))
-                asset_year_rep_period_dict[key] = _get_indices_for_sum(
-                    connection,
-                    table_name,
-                    row.asset,
-                    row.milestone_year,
-                    row.rep_period,
-                    "shut_down",
-                )
-            end
-        end
-
-        expr_avail_simple_method =
-            expressions[:available_asset_units_aggregated_vintage_method].expressions[:assets]
-
-        indices =
-            _append_available_units_shut_down_aggregated_vintage_method(connection, table_name)
+        indices = _get_indices_for_minimum_down_time_constraints_aggregated_vintage_method(
+            connection,
+            table_name,
+        )
 
         attach_constraint!(
             model,
@@ -40,11 +26,8 @@ function add_minimum_down_time_constraints!(connection, model, variables, expres
             [
                 @constraint(
                     model,
-                    _sum_min_down_blocks(
-                        asset_year_rep_period_dict["$(row.asset),$(row.milestone_year),$(row.rep_period)"],
-                        variables[:shut_down].container,
-                        row.time_block_start,
-                    ) <= expr_avail_simple_method[row.avail_id] - units_on[row.units_on_id],
+                    sum(shut_down[i] for i in row.first_shut_down_id:row.last_shut_down_id) <=
+                    expr_avail_aggregated_vintage_method[row.avail_id] - units_on[row.units_on_id],
                     base_name = "$table_name[$(row.asset),$(row.milestone_year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
                 ) for row in indices
             ],
@@ -53,29 +36,16 @@ function add_minimum_down_time_constraints!(connection, model, variables, expres
 
     #Minimum down time with compact investment strategy
     let table_name = :minimum_down_time_compact_vintage_method,
-        units_on = variables[:units_on].container
-
+        units_on = variables[:units_on].container,
+        shut_down = variables[:shut_down].container,
+        expr_avail_compact_method =
+            expressions[:available_asset_units_compact_vintage_method].expressions[:assets],
         cons = constraints[table_name]
 
-        asset_year_rep_period_dict = Dict()
-        for row in cons.indices
-            key = "$(row.asset),$(row.milestone_year),$(row.rep_period)"
-            if (!haskey(asset_year_rep_period_dict, key))
-                asset_year_rep_period_dict[key] = _get_indices_for_sum(
-                    connection,
-                    table_name,
-                    row.asset,
-                    row.milestone_year,
-                    row.rep_period,
-                    "shut_down",
-                )
-            end
-        end
-
-        expr_avail_compact_method =
-            expressions[:available_asset_units_compact_vintage_method].expressions[:assets]
-
-        indices = _append_available_units_shut_down_compact_method(connection, table_name)
+        indices = _get_indices_for_minimum_down_time_constraints_compact_vintage_method(
+            connection,
+            table_name,
+        )
 
         attach_constraint!(
             model,
@@ -84,11 +54,7 @@ function add_minimum_down_time_constraints!(connection, model, variables, expres
             [
                 @constraint(
                     model,
-                    _sum_min_down_blocks(
-                        asset_year_rep_period_dict["$(row.asset),$(row.milestone_year),$(row.rep_period)"],
-                        variables[:shut_down].container,
-                        row.time_block_start,
-                    ) <=
+                    sum(shut_down[i] for i in row.first_shut_down_id:row.last_shut_down_id) <=
                     sum(expr_avail_compact_method[avail_id] for avail_id in row.avail_indices) -
                     units_on[row.units_on_id],
                     base_name = "$table_name[$(row.asset),$(row.milestone_year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
@@ -98,18 +64,121 @@ function add_minimum_down_time_constraints!(connection, model, variables, expres
     end
 end
 
-function _sum_min_down_blocks(sum_rows, shut_downs, start_of_curr_constraint)
-    sum = 0
+function _get_indices_for_minimum_down_time_constraints_aggregated_vintage_method(
+    connection,
+    table_name,
+)
+    return DuckDB.query(
+        connection,
+        "WITH cons_with_lower_bound AS (
+            SELECT
+                cons.*,
+                cons.time_block_start - CAST(CEIL(asset.minimum_down_time::DOUBLE / rep_periods_data.resolution::DOUBLE) AS INTEGER) + 1 AS lower_bound
+            FROM cons_$table_name as cons
+            LEFT JOIN asset ON
+                asset.asset = cons.asset
+            LEFT JOIN rep_periods_data
+                ON rep_periods_data.milestone_year = cons.milestone_year
+                AND rep_periods_data.rep_period = cons.rep_period
+            WHERE asset.vintage_method = 'aggregated'
+        )
+        SELECT
+            clb.id,
+            clb.asset,
+            clb.milestone_year,
+            clb.rep_period,
+            clb.time_block_start,
+            ANY_VALUE(clb.time_block_end) AS time_block_end,
+            ANY_VALUE(var_units_on.id) as units_on_id,
+            ARG_MIN(var_shut_down.id, var_shut_down.time_block_start) FILTER (WHERE var_shut_down.time_block_start >= clb.lower_bound) AS first_shut_down_id,
+            ANY_VALUE(var_shut_down.id) FILTER (WHERE var_shut_down.time_block_start = clb.time_block_start) AS last_shut_down_id,
+            ANY_VALUE(expr_avail.id) AS avail_id
+        FROM cons_with_lower_bound as clb
+        LEFT JOIN var_shut_down ON
+            var_shut_down.asset = clb.asset AND
+            var_shut_down.milestone_year = clb.milestone_year AND
+            var_shut_down.rep_period = clb.rep_period
+        LEFT JOIN var_units_on ON
+            var_units_on.asset = clb.asset AND
+            var_units_on.milestone_year = clb.milestone_year AND
+            var_units_on.rep_period = clb.rep_period AND
+            var_units_on.time_block_start = clb.time_block_start
+        LEFT JOIN expr_available_asset_units_aggregated_vintage_method AS expr_avail
+            ON clb.asset = expr_avail.asset
+            AND clb.milestone_year = expr_avail.milestone_year
+        GROUP BY
+            clb.id,
+            clb.asset,
+            clb.milestone_year,
+            clb.rep_period,
+            clb.time_block_start
+        ORDER BY clb.id",
+    )
+end
 
-    for single_row in sum_rows
-        minimum_down_steps = ceil(Int, single_row.minimum_down_time / single_row.resolution)
-        lower_bound = start_of_curr_constraint - minimum_down_steps + 1
-
-        start_of_this = single_row.time_block_start
-
-        if (lower_bound <= start_of_this <= start_of_curr_constraint)
-            sum = sum + shut_downs[single_row.shut_down_id]
-        end
-    end
-    return sum
+function _get_indices_for_minimum_down_time_constraints_compact_vintage_method(
+    connection,
+    table_name,
+)
+    return DuckDB.query(
+        connection,
+        "WITH cons_with_lower_bound AS (
+            SELECT
+                cons.*,
+                cons.time_block_start - CAST(CEIL(asset.minimum_up_time::DOUBLE / rep_periods_data.resolution::DOUBLE) AS INTEGER) + 1 AS lower_bound
+            FROM cons_$table_name as cons
+            LEFT JOIN asset ON
+                asset.asset = cons.asset
+            LEFT JOIN rep_periods_data
+                ON rep_periods_data.milestone_year = cons.milestone_year
+                AND rep_periods_data.rep_period = cons.rep_period
+            WHERE asset.vintage_method = 'compact_profiles'
+        ),
+        cons_with_shut_down_ids AS (
+            SELECT
+                clb.id,
+                clb.asset,
+                clb.milestone_year,
+                clb.rep_period,
+                clb.time_block_start,
+                ANY_VALUE(clb.time_block_end) AS time_block_end,
+                ANY_VALUE(var_units_on.id) as units_on_id,
+                ARG_MIN(var_shut_down.id, var_shut_down.time_block_start) FILTER (WHERE var_shut_down.time_block_start >= clb.lower_bound) AS first_shut_down_id,
+                ANY_VALUE(var_shut_down.id) FILTER (WHERE var_shut_down.time_block_start = clb.time_block_start) AS last_shut_down_id
+            FROM cons_with_lower_bound as clb
+            LEFT JOIN var_shut_down ON
+                var_shut_down.asset = clb.asset AND
+                var_shut_down.milestone_year = clb.milestone_year AND
+                var_shut_down.rep_period = clb.rep_period
+            LEFT JOIN var_units_on ON
+                var_units_on.asset = clb.asset AND
+                var_units_on.milestone_year = clb.milestone_year AND
+                var_units_on.rep_period = clb.rep_period AND
+                var_units_on.time_block_start = clb.time_block_start
+            GROUP BY
+                clb.id,
+                clb.asset,
+                clb.milestone_year,
+                clb.rep_period,
+                clb.time_block_start
+        )
+        SELECT
+            csd.id,
+            ANY_VALUE(csd.asset) AS asset,
+            ANY_VALUE(csd.milestone_year) AS milestone_year,
+            ANY_VALUE(csd.rep_period) AS rep_period,
+            ANY_VALUE(csd.time_block_start) as time_block_start,
+            ANY_VALUE(csd.time_block_end) as time_block_end,
+            ANY_VALUE(csd.units_on_id) as units_on_id,
+            ANY_VALUE(csd.first_shut_down_id) as first_shut_down_id,
+            ANY_VALUE(csd.last_shut_down_id) as last_shut_down_id,
+            ARRAY_AGG(expr_avail.id) AS avail_indices
+        FROM cons_with_shut_down_ids AS csd
+        LEFT JOIN expr_available_asset_units_compact_vintage_method AS expr_avail
+            ON csd.asset = expr_avail.asset
+            AND csd.milestone_year = expr_avail.milestone_year
+        GROUP BY csd.id
+        ORDER BY csd.id
+        ",
+    )
 end
--- src/constraints/unit-commitment-minimum-up-time.jl
+++ src/constraints/unit-commitment-minimum-up-time.jl
@@ -9,22 +9,8 @@ function add_minimum_up_time_constraints!(connection, model, variables, expressi
     let table_name = :minimum_up_time,
         cons = constraints[:minimum_up_time],
         units_on = variables[:units_on].container,
-        indices = _append_variable_ids(connection, table_name, ["units_on"])
-
-        asset_year_rep_period_dict = Dict()
-        for row in cons.indices
-            key = "$(row.asset),$(row.milestone_year),$(row.rep_period)"
-            if (!haskey(asset_year_rep_period_dict, key))
-                asset_year_rep_period_dict[key] = _get_indices_for_sum(
-                    connection,
-                    table_name,
-                    row.asset,
-                    row.milestone_year,
-                    row.rep_period,
-                    "start_up",
-                )
-            end
-        end
+        start_up = variables[:start_up].container,
+        indices = _get_indices_for_minimum_up_time_constraints(connection, table_name)
 
         attach_constraint!(
             model,
@@ -33,11 +19,8 @@ function add_minimum_up_time_constraints!(connection, model, variables, expressi
             [
                 @constraint(
                     model,
-                    _sum_min_up_blocks(
-                        asset_year_rep_period_dict["$(row.asset),$(row.milestone_year),$(row.rep_period)"],
-                        variables[:start_up].container,
-                        row.time_block_start,
-                    ) <= units_on[row.units_on_id],
+                    sum(start_up[i] for i in row.first_start_up_id:row.last_start_up_id) <=
+                    units_on[row.units_on_id],
                     base_name = "$table_name[$(row.asset),$(row.milestone_year),$(row.rep_period),$(row.time_block_start):$(row.time_block_end)]"
                 ) for row in indices
             ],
@@ -45,52 +28,46 @@ function add_minimum_up_time_constraints!(connection, model, variables, expressi
     end
 end
 
-function _sum_min_up_blocks(sum_rows, start_ups, start_of_curr_constraint)
-    sum = 0
-    for single_row in sum_rows
-        minimum_up_steps = ceil(Int, single_row.minimum_up_time / single_row.resolution)
-        lower_bound = start_of_curr_constraint - minimum_up_steps + 1
-
-        start_of_this = single_row.time_block_start
-
-        if (lower_bound <= start_of_this <= start_of_curr_constraint)
-            sum = sum + start_ups[single_row.start_up_id]
-        end
-    end
-    return sum
-end
-
-function _get_indices_for_sum(
-    connection,
-    table_name,
-    curr_asset,
-    curr_year,
-    curr_rep_period,
-    variable_name,
-)
-    variable_table_name = "var_" * variable_name
-
+function _get_indices_for_minimum_up_time_constraints(connection, table_name)
     return DuckDB.query(
         connection,
-        "SELECT
-            cons.*,
-            asset.minimum_up_time,
-            asset.minimum_down_time,
-            $variable_table_name.id as $(variable_name)_id,
-            rep_periods_data.resolution as resolution
-        FROM cons_$table_name AS cons
-        LEFT JOIN asset
-            ON cons.asset = asset.asset
-        LEFT JOIN $variable_table_name
-            ON $variable_table_name.asset = cons.asset
-            AND $variable_table_name.milestone_year = cons.milestone_year
-            AND $variable_table_name.rep_period = cons.rep_period
-            AND $variable_table_name.time_block_start = cons.time_block_start
-        LEFT JOIN rep_periods_data
-            ON rep_periods_data.milestone_year == cons.milestone_year
-            AND rep_periods_data.rep_period == cons.rep_period
-        WHERE cons.asset = '$curr_asset' AND cons.milestone_year = $curr_year AND cons.rep_period = $curr_rep_period
-        ORDER BY cons.id
-        ",
+        "WITH cons_with_lower_bound AS (
+            SELECT
+                cons.*,
+                cons.time_block_start - CAST(CEIL(asset.minimum_up_time::DOUBLE / rep_periods_data.resolution::DOUBLE) AS INTEGER) + 1 AS lower_bound
+            FROM cons_$table_name as cons
+            LEFT JOIN asset ON
+                asset.asset = cons.asset
+            LEFT JOIN rep_periods_data
+                ON rep_periods_data.milestone_year = cons.milestone_year
+                AND rep_periods_data.rep_period = cons.rep_period
+        )
+        SELECT
+            clb.id,
+            clb.asset,
+            clb.milestone_year,
+            clb.rep_period,
+            clb.time_block_start,
+            ANY_VALUE(clb.time_block_end) AS time_block_end,
+            ANY_VALUE(var_units_on.id) as units_on_id,
+            ARG_MIN(var_start_up.id, var_start_up.time_block_start) FILTER (WHERE var_start_up.time_block_start >= clb.lower_bound) AS first_start_up_id,
+            ANY_VALUE(var_start_up.id) FILTER (WHERE var_start_up.time_block_start = clb.time_block_start) AS last_start_up_id
+        FROM cons_with_lower_bound as clb
+        LEFT JOIN var_start_up ON
+            var_start_up.asset = clb.asset AND
+            var_start_up.milestone_year = clb.milestone_year AND
+            var_start_up.rep_period = clb.rep_period
+        LEFT JOIN var_units_on ON
+            var_units_on.asset = clb.asset AND
+            var_units_on.milestone_year = clb.milestone_year AND
+            var_units_on.rep_period = clb.rep_period AND
+            var_units_on.time_block_start = clb.time_block_start
+        GROUP BY
+            clb.id,
+            clb.asset,
+            clb.milestone_year,
+            clb.rep_period,
+            clb.time_block_start
+        ORDER BY clb.id",
     )
 end
